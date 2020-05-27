27725a065f17390ab6c6b9cd65e4a4e023cdfc95
alichaddad <chaddada@greenitglobe.com>
2020-05-27 15:46:14 +0200

    use toml for package defintion

--- jumpscale/packages/chatflows/bottle/bottle.py
+++ jumpscale/packages/chatflows/bottle/bottle.py
@@ -8,19 +8,16 @@ app = Bottle()
 templates_path = j.sals.fs.join_paths(j.sals.fs.dirname(__file__), "..", "frontend")
 env = Environment(loader=FileSystemLoader(templates_path))
 
-threebot = j.servers.threebot.get('default')
+threebot = j.servers.threebot.get("default")
 
-@app.route('/<package_name>')
+
+@app.route("/<package_name>")
 def chats(package_name):
     package = threebot.packages.get(package_name)
     chatflows = package.get_chats()
     return j.data.serializers.json.dumps(list(chatflows.keys()))
 
 
-@app.route('/<package_name>/<chat_name>')
+@app.route("/<package_name>/<chat_name>")
 def chat(package_name, chat_name):
-    return env.get_template("index.html").render(
-        topic=chat_name,
-        username="",
-        email=""
-    )
+    return env.get_template("index.html").render(topic=chat_name, username="", email="")
