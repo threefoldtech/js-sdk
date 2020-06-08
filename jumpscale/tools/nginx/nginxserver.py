72b90abf14c87fe16711c4e04fcd2b868ac181ef
BolaNasr <bola.nasr@live.com>
2020-06-08 17:23:41 +0200

    add doc string to nginx tools

--- jumpscale/tools/nginx/nginxserver.py
+++ jumpscale/tools/nginx/nginxserver.py
@@ -7,12 +7,18 @@ class NginxServer(Base):
     config_path = fields.String(default="~/sandbox/cfg/nginx/main/nginx.conf")
 
     def install(self):
+        """
+        install nginx and certbot
+        """
         j.sals.process.execute("add-apt-repository ppa:certbot/certbot", showout=True, die=False)
         j.sals.process.execute(
             "apt-get install -y nginx certbot python-certbot-nginx ", showout=True, die=False,
         )
 
     def start(self):
+        """
+        start nginx server using your config path
+        """
         nginx = j.sals.nginx.get(self.name)
         nginx.configure()
         nginx.save()
@@ -22,12 +28,21 @@ class NginxServer(Base):
         cmd.start()
 
     def stop(self):
+        """
+        stop nginx server
+        """
         cmd = j.tools.startupcmd.get(self.name)
         cmd.stop()
 
     def reload(self):
+        """
+        reload nginx server using your config path
+        """
         j.sals.process.execute(f"nginx -c {self.config_path} -s reload")
 
     def restart(self):
+        """
+        restart nginx server
+        """
         self.stop()
         self.start()
