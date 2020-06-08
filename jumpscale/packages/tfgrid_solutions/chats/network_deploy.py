3f642fc8a645b3805c1430768c603ad630e545ce
m-motawea <magedmotawea@gmail.com>
2020-06-08 17:09:19 +0200

    add network chatflow steps

--- jumpscale/packages/tfgrid_solutions/chats/network_deploy.py
+++ jumpscale/packages/tfgrid_solutions/chats/network_deploy.py
@@ -1,6 +1,7 @@
 from jumpscale.servers.gedis.baseactor import BaseActor, actor_method
 from jumpscale.god import j
 from jumpscale.sals.chatflows.chatflows import GedisChatBot, chatflow_step, StopChatFlow
+from jumpscale.sals.reservation_chatflow.models import SolutionType
 import time
 
 
@@ -20,8 +21,8 @@ class NetworkDeploy(GedisChatBot):
         expiration = self.datetime_picker(
             "Please enter network expiration time.",
             required=True,
-            # min_time=[3600, "Date/time should be at least 1 hour from now"],
-            # default=j.data.time.utcnow().timestamp + 3900,
+            min_time=[3600, "Date/time should be at least 1 hour from now"],
+            default=j.data.time.utcnow().timestamp + 3900,
         )
 
         ips = ["IPv6", "IPv4"]
@@ -30,6 +31,55 @@ class NetworkDeploy(GedisChatBot):
             ips, required=True
         )
         user_form_data["Solution expiration"] = j.data.time.get(expiration).humanize()
+
+        reservation = j.sals.zos.reservation_create()
+        ip_range = j.sals.reservation_chatflow.get_ip_range(self)
+        res = j.sals.reservation_chatflow.get_solution_metadata(network_name, SolutionType.Network, user_form_data)
+        reservation = j.sals.reservation_chatflow.add_reservation_metadata(reservation, res)
+        
+        while True:
+            self.config = j.sals.reservation_chatflow.create_network(
+                network_name,
+                reservation,
+                ip_range,
+                j.core.identity.tid,
+                ipversion,
+                expiration=expiration,
+                currency=user_form_data["Currency"],
+                bot=self,
+            )
+            try:
+                j.sal.reservation_chatflow.register_and_pay_reservation(self.config["reservation_create"], bot=self)
+                break
+            except StopChatFlow as e:
+                if "wireguard listen port already in use" in e.msg:
+                    j.sals.zos.reservation_cancel(self.config["rid"])
+                    time.sleep(5)
+                    continue
+                raise
+
+    @chatflow_step(title="Network Information", disable_previous=True)
+    def network_info(self):
+        print(self.config)
+        message = """
+### Use the following template to configure your wireguard connection. This will give you access to your network.
+#### Make sure you have <a target="_blank" href="https://www.wireguard.com/install/">wireguard</a> installed
+Click next
+to download your configuration
+        """
+
+        self.md_show(message, md=True, html=True)
+
+        filename = "wg-{}.conf".format(self.config["rid"])
+        self.download_file(msg=f'<pre>{self.config["wg"]}</pre>', data=self.config["wg"], filename=filename, html=True)
+
+        message = f"""
+### In order to have the network active and accessible from your local/container machine. To do this, execute this command:
+#### ```wg-quick up /etc/wireguard/{filename}```
+# Click next
+        """
+
+        self.md_show(message, md=True)
 
 
 chat = NetworkDeploy
