89569783bda7767cb9cb77a10532bc18adab1263
AhmedSa-mir <asamir97@hotmail.com>
2020-06-08 12:39:19 +0200

    Implemented set_explorer actor in admin_admin

--- jumpscale/packages/admin/actors/admin.py
+++ jumpscale/packages/admin/actors/admin.py
@@ -44,6 +44,30 @@ class Admin(BaseActor):
 
     @actor_method
     def set_explorer(self, explorer_type: str) -> str:
-        return "hello from set_explorer actor"
+        if explorer_type in explorers:
+            cfg = j.core.config.get("threebot")
+            client = j.clients.explorer.get(
+                name=explorer_type,
+                url=f"https://{explorers[explorer_type]}/explorer")
+            
+            # check if we can switch with existing identity
+            try:
+                user = client.users.get(name=cfg["name"], email=cfg["email"])
+            except j.exceptions.NotFound:
+                return j.data.serializers.json.dumps(
+                    f"Your identity does not exists on {explorer_type}")
+            if user.pubkey != j.core.identity.nacl.get_verify_key_hex():
+                return j.data.serializers.json.dumps(
+                    f"Your identity does not match on {explorer_type}")
+            
+            # cfg["id"] = user.id
+            j.clients.explorer.default_addr_set(explorers[explorer_type])
+
+            # update our solutions
+            # j.sal.reservation_chatflow.solutions_explorer_get()
+            return j.data.serializers.json.dumps(
+                {"type": explorer_type, "url": explorers[explorer_type]})
+        return j.data.serializers.json.dumps(
+                f"{explorer_type} is not a valid explorer type, must be 'testnet' or 'main'")
 
 Actor = Admin
