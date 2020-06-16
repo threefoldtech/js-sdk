da0c714414e51ca30a67bd20c747d6361c0db1e0
AhmedSa-mir <asamir97@hotmail.com>
2020-06-16 15:21:22 +0200

    Modified external view to include required packages

--- jumpscale/packages/admin/frontend/frontend_src/sources/views/external/index.js
+++ jumpscale/packages/admin/frontend/frontend_src/sources/views/external/index.js
@@ -1,19 +1,17 @@
 import { JetView } from "webix-jet";
-
-
-// TODO: Add required packages
+import { packages } from "../../services/packages"
 
 export class ExternalView extends JetView {
-    constructor(app, name, targetUrl) {
+    constructor(app, name, targetUrl, requiredPackages) {
         super(app, name);
 
         this.targetUrl = targetUrl || "/";
+        this.requiredPackages = requiredPackages || {}; // required packages as (name: git_url) pairs
     }
 
     config() {
         const self = this;
-
-        return {
+        const iframe = {
             view: "iframe",
             localId: "iframe-external",
             on: {
@@ -24,7 +22,53 @@ export class ExternalView extends JetView {
                     this.enable();
                 }
             }
+        };
+
+        return {
+            rows: [{
+                localId: "install-packages",
+                hidden: true,
+                cols: [
+                    {
+                        localId: "required_packages_div",
+                        view: "template",
+                        autoheight: true,
+                    }, {
+                        view: "button",
+                        localId: "install_btn",
+                        value: "Install required packages",
+                        css: "webix_primary",
+                        height: 50,
+                        click: self.installRequiredPackages.bind(self)
+                    }, {
+                        view: "button",
+                        localId: "go_to_packages_btn",
+                        value: "Go to packages and install them manually",
+                        css: "webix_primary",
+                        height: 50,
+                        click: function () {
+                            this.$scope.show("/main/packages");
+                        }
+                    }
+                ]
+            }, iframe]
         }
+    }
+
+    installRequiredPackages() {
+        // let promises = Object.values(this.packagesToInstall).map((path) => {
+        //     return packages.add(null, path); // add by git url
+        // });
+
+        // this.installButton.disable();
+
+        // Promise.all(promises).then(() => {
+        packages.add(null, Object.values(this.packagesToInstall)[0]).then(() => {
+            webix.message({ type: "success", text: "All required packages installed successfully, page will be reloaded in 2 seconds" });
+            setInterval(() => window.location.reload(true), 2000);
+        }).catch(() => {
+            webix.message({ type: "error", text: "An error occurred, please try installing from packages for more details" });
+        });
     }
 
     showIframe() {
@@ -38,6 +82,48 @@ export class ExternalView extends JetView {
         this.externalIframe.disable();
         webix.extend(this.externalIframe, webix.ProgressBar);
 
-        this.showIframe();
+        this.packageNames = Object.keys(this.requiredPackages); // only names
+
+        if (!this.packageNames.length) {
+            this.showIframe();
+            return;
+        }
+
+        this.requiredPackagesDiv = this.$$("required_packages_div");
+        this.installPackageContainer = this.$$("install-packages");
+        this.installButton = this.$$("install_btn");
+
+        this.packagesToInstall = {};
+
+        // check which packages to install
+        // if any is already installed, then just ignore it
+        packages.list().then(data => {
+            const packageList = JSON.parse(data.json()).data;
+
+            // now go over required packages
+            for (let name of this.packageNames) {
+                // check if a required package is already installed
+                if (name in packageList) {
+                    continue;
+                }
+
+                this.packagesToInstall[name] = this.requiredPackages[name];
+            }
+
+            // check packages to be installed again if still need to install any of them
+            const packageNamesToInstall = Object.keys(this.packagesToInstall);
+            if (packageNamesToInstall.length) {
+                this.installPackageContainer.show();
+                this.externalIframe.hide();
+
+                const names = packageNamesToInstall.join(", ");
+                this.requiredPackagesDiv.setHTML(
+                    `<div style='width:auto;text-align:center'><h3>You need to install the following required packages: ${names}<h3/></div>`
+                );
+            } else {
+                this.installPackageContainer.hide();
+                this.showIframe()
+            }
+        });
     }
 }
--- jumpscale/packages/admin/frontend/frontend_src/sources/views/farmmanagement/index.js
+++ jumpscale/packages/admin/frontend/frontend_src/sources/views/farmmanagement/index.js
@@ -1,10 +1,13 @@
 import { ExternalView } from "../external";
 
+// TODO: modify url to be development branch
 const URL = "/farmmanagement/frontend/";
-// TODO: Add required package
+const REQUIRED_PACKAGES = {
+    "farmmanagement": "https://github.com/threefoldtech/js-sdk/blob/development_adminpanel/jumpscale/packages/farmmanagement"
+}
 
 export default class FarmmanagementView extends ExternalView {
     constructor(app, name) {
-        super(app, name, URL);
+        super(app, name, URL, REQUIRED_PACKAGES);
     }
 }
