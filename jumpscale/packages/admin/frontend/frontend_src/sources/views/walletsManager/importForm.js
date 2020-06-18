d746e43444496a769165788475fd26b0ca298b04
MAhmoud Ali <dmahmou213@gmail.com>
2020-06-18 19:57:53 +0200

    Added cancel functionality to wallet popups

--- jumpscale/packages/admin/frontend/frontend_src/sources/views/walletsManager/importForm.js
+++ jumpscale/packages/admin/frontend/frontend_src/sources/views/walletsManager/importForm.js
@@ -8,8 +8,7 @@ export default class WalletImportView extends JetView {
             view: "form",
             id: "import_form",
             elementsConfig: { labelWidth: 200 },
-            elements: [
-                {
+            elements: [{
                     view: "text",
                     label: "Name",
                     name: "name",
@@ -22,20 +21,21 @@ export default class WalletImportView extends JetView {
                     placeholder: "Wallet secret"
                 },
                 {
-                view:"select", 
-                label:"Network Type", 
-                value: "network",
-                name: "network",
-                options:[
-                    { "id":"STD", "value":"STD" },
-                    { "id":"TEST", "value":"TEST" }
-                ]
-            }
+                    view: "select",
+                    label: "Network Type",
+                    value: "network",
+                    name: "network",
+                    options: [
+                        { "id": "STD", "value": "STD" },
+                        { "id": "TEST", "value": "TEST" }
+                    ]
+                }
             ]
         };
 
         return {
             view: "window",
+            id: "popupWindow",
             head: "Import wallet",
             modal: true,
             width: window.innerWidth * .8,
@@ -45,18 +45,24 @@ export default class WalletImportView extends JetView {
                 rows: [
                     info,
                     {
-                        view: "button",
-                        value: "OK",
-                        css: "webix_primary",
-                        click: function () {
-                            var name = $$('import_form').getValues().name
-                            var secret = $$('import_form').getValues().secret
-                            var network = $$('import_form').getValues().network
-                            console.log(name);
-                            console.log(secret);
-                            console.log(network);
-                            this.$scope.importWallet(name, secret, network);
-                        }
+                        cols: [{
+                            view: "button",
+                            value: "Close",
+                            css: "webix_secondary",
+                            click: function() {
+                                this.getTopParentView().hide();
+                            }
+                        }, {
+                            view: "button",
+                            value: "OK",
+                            css: "webix_primary",
+                            click: function() {
+                                var name = $$('import_form').getValues().name
+                                var secret = $$('import_form').getValues().secret
+                                var network = $$('import_form').getValues().network
+                                this.$scope.importWallet(name, secret, network);
+                            }
+                        }]
                     }
                 ]
             }
@@ -65,6 +71,11 @@ export default class WalletImportView extends JetView {
 
     init() {
         this.form = $$("import_form");
+        $$("popupWindow").attachEvent("onKeyPress", function(code, e) {
+            if (code == 27) {
+                this.getTopParentView().hide();
+            }
+        });
     }
 
     showForm() {
@@ -74,19 +85,19 @@ export default class WalletImportView extends JetView {
     importWallet(name, secret, network) {
         webix.extend(this.form, webix.ProgressBar);
         this.form.showProgress({
-            type:"icon",
+            type: "icon",
             hide: false
         });
         wallet.importWallet(name, secret, network).then(data => {
             webix.message({ type: "success", text: "Wallet imported successfully" });
-            this.form.showProgress({hide: true});
-            this.form.clear(); 
+            this.form.showProgress({ hide: true });
+            this.form.clear();
             this.form.getTopParentView().hide();
             this.app.refresh()
         }).catch(error => {
             webix.message({ type: "error", text: "Could not import wallet" });
-            this.form.showProgress({hide: true});
-            this.form.clear(); 
+            this.form.showProgress({ hide: true });
+            this.form.clear();
             this.form.getTopParentView().hide();
             this.app.refresh()
         });
--- jumpscale/packages/admin/frontend/frontend_src/sources/views/walletsManager/walletForm.js
+++ jumpscale/packages/admin/frontend/frontend_src/sources/views/walletsManager/walletForm.js
@@ -7,26 +7,24 @@ export default class WalletFormView extends JetView {
             view: "form",
             id: "form",
             elementsConfig: { labelWidth: 200 },
-            elements: [
-                {
-                    view: "text",
-                    label: "Name",
-                    name: "name",
-                    placeholder: "Wallet name"
-                }
-            ]
+            elements: [{
+                view: "text",
+                label: "Name",
+                name: "name",
+                placeholder: "Wallet name"
+            }]
         };
 
         return {
             view: "window",
+            id: "popupWindow",
             head: "Create new wallet",
             modal: true,
             width: window.innerWidth * .8,
             height: window.innerHeight * .8,
             position: "center",
             body: {
-                rows: [
-                    {
+                rows: [{
                         view: "template",
                         css: "wallet-warning",
                         height: 30,
@@ -34,13 +32,22 @@ export default class WalletFormView extends JetView {
                     },
                     info,
                     {
-                        view: "button",
-                        value: "OK",
-                        css: "webix_primary",
-                        click: function () {
-                            var wallet_name = $$('form').getValues().name
-                            this.$scope.createWallet(wallet_name);
-                        }
+                        cols: [{
+                            view: "button",
+                            value: "Close",
+                            css: "webix_secondary",
+                            click: function() {
+                                this.getTopParentView().hide();
+                            }
+                        }, {
+                            view: "button",
+                            value: "OK",
+                            css: "webix_primary",
+                            click: function() {
+                                var wallet_name = $$('form').getValues().name
+                                this.$scope.createWallet(wallet_name);
+                            }
+                        }]
                     }
                 ]
             }
@@ -49,26 +56,31 @@ export default class WalletFormView extends JetView {
 
     init() {
         this.form = $$("form");
+        $$("popupWindow").attachEvent("onKeyPress", function(code, e) {
+            if (code == 27) {
+                this.getTopParentView().hide();
+            }
+        });
     }
 
     showForm() {
         this.getRoot().show();
     }
 
-    createWallet(name){
+    createWallet(name) {
         webix.extend(this.form, webix.ProgressBar);
         this.form.showProgress({
-            type:"icon",
+            type: "icon",
             hide: false
         });
         wallet.createWallet(name).then(data => {
             webix.message({ type: "success", text: "Wallet created successfully" });
-            this.form.showProgress({hide: true});
+            this.form.showProgress({ hide: true });
             this.form.getTopParentView().hide();
             this.app.refresh()
         }).catch(error => {
-            webix.message({ type: "error", text: "Could not create wallet", expire: -1});
-            this.form.showProgress({hide: true});
+            webix.message({ type: "error", text: "Could not create wallet", expire: -1 });
+            this.form.showProgress({ hide: true });
             this.form.getTopParentView().hide();
             this.app.refresh()
         });
