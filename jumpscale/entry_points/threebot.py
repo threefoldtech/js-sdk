aa6020618a7509b5f1f9e13d1ab975e0011fce1f
Waleed <waleed.hammam@gmail.com>
2020-06-25 11:19:36 +0200

    add waiting time for threebot server to start

--- jumpscale/entry_points/threebot.py
+++ jumpscale/entry_points/threebot.py
@@ -68,9 +68,14 @@ def start(identity=None):
     cmd.process_strings_regex = [j.tools.nginx.get("default").check_command_string, "nginx.*"]
     cmd.ports = [8000, 8999]
     cmd.start()
-    print("\n✅ Threebot server started\n")
-    if j.sals.process.in_host():
-        j.tools.console.printcolors("{WHITE}Visit admin dashboard at: {GREEN}http://localhost/\n{RESET}")
+
+    if j.sals.nettools.tcp_connection_test(ipaddr="localhost", port=SERVICES_PORTS["gedis"], timeout=15):
+        print("\n✅ Threebot server started\n")
+        if j.sals.process.in_host():
+            j.tools.console.printcolors("{WHITE}Visit admin dashboard at: {GREEN}http://localhost/\n{RESET}")
+    else:
+        j.tools.console.printcolors("{RED}Could not start threebot server in 15 seconds, Please try again{RESET}")
+        sys.exit(1)
 
 
 @click.command()
