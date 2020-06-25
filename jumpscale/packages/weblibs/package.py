c6f38e88a8cc30d61f766ba4ebf6bd7018bb1330
MAhmoud Ali <dmahmou213@gmail.com>
2020-06-25 13:57:30 +0200

    refactored weiblibs cloning

--- jumpscale/packages/weblibs/package.py
+++ jumpscale/packages/weblibs/package.py
@@ -11,15 +11,14 @@ class weblibs:
         """Called when package is added
         """
         if not j.sals.fs.exists(j.sals.fs.join_paths(self.path, "jumpscaleX_weblibs")):
-            retries = 2
+            retries = 3
             while retries:
                 try:
                     j.tools.git.clone_repo(url=self.url, dest=self.path, branch_or_tag=self.branch, depth=1)
-                    break
+                    return
                 except Exception:
-                    if retries == 1:
-                        raise j.exceptions.Timeout("Clone weblibs repo failed")
                     retries = retries - 1
+            raise j.exceptions.Timeout("Clone weblibs repo failed")
 
     def uninstall(self):
         """Called when package is deleted
