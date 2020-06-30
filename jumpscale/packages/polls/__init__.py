98370f906856206da514b723e61917d4384e4900
Waleed <waleed.hammam@gmail.com>
2020-06-30 19:29:23 +0200

    update polls package docs

---
+++ jumpscale/packages/polls/__init__.py
@@ -0,0 +1,24 @@
+"""Polls Package
+
+## Installation
+
+- Configure a wallet using stellar client named "polls_receive" and update the trustlines see: [Quick Start](https://github.com/threefoldtech/js-sdk/blob/9411bf6a359c88994475d4f90c412c1112d3487d/docs/wiki/quick_start.md#L77)
+
+- and its name tp polls.py
+  ```python
+  WALLET_NAME = "polls_receive"
+  ```
+
+- Start 3bot server and add the package
+
+- polls now available at http://localhost/polls/chats/<poll_name>
+
+
+## Create new poll
+
+- check one of the examples
+
+  - Default questions will be added in self.QUESTIONS variable
+  - custom vote allows you to add your custom questions just update the dicts and return of it
+
+"""
