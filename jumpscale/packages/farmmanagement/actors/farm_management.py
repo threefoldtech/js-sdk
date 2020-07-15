134902bd289d873bcfddf8c7c2d72e4225f33409
Waleed <waleed.hammam@gmail.com>
2020-07-15 22:50:29 +0200

    fix default explorer to be taken from the identity rather than client

--- jumpscale/packages/farmmanagement/actors/farm_management.py
+++ jumpscale/packages/farmmanagement/actors/farm_management.py
@@ -2,10 +2,11 @@ from jumpscale.servers.gedis.baseactor import BaseActor, actor_method
 from jumpscale.clients.explorer.models import TfgridDirectoryFarm1
 from jumpscale.loader import j
 
+
 class FarmManagemenet(BaseActor):
     def __init__(self):
         super().__init__()
-        self._explorer = j.clients.explorer.get_default()
+        self._explorer = j.core.identity.me.explorer
 
     @actor_method
     def update_farm(self, farm_id, farm):
@@ -16,5 +17,6 @@ class FarmManagemenet(BaseActor):
     @actor_method
     def mark_node_free(self, node_id, free):
         return self._explorer.nodes.configure_free_to_use(node_id=node_id, free=free)
+
 
 Actor = FarmManagemenet
