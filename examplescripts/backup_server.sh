71f8a9ecf053f082978a50e777b191d8c42833fe
BolaNasr <bola.nasr@live.com>
2020-07-20 14:58:45 +0200

    get update in script

--- examplescripts/backup_server.sh
+++ examplescripts/backup_server.sh
@@ -6,7 +6,7 @@ gzip -d rest-server-0.9.7-linux-amd64.gz
 
 chmod +x rest-server-0.9.7-linux-amd64
 mv /root/rest-server-0.9.7-linux-amd64 /usr/bin/rest-server
-
+apt-get update
 apt-get install -y apache2-utils
 mkdir ~/backup
 touch ~/backup/.htpasswd
