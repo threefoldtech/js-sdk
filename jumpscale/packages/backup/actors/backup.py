c60df1997cb1c80f76172ea4f438cdc2333ac413
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2020-07-21 15:47:17 +0200

    Adds backup package
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

---
+++ jumpscale/packages/backup/actors/backup.py
@@ -0,0 +1,111 @@
+from jumpscale.loader import j
+from jumpscale.servers.gedis.baseactor import BaseActor, actor_method
+import os
+import requests
+import nacl.encoding
+import nacl
+from nacl.public import Box
+import binascii
+import time
+import os
+MARKETPLACE_URL = os.environ.get('MARKETPLACE_URL', "https://marketplace.threefold.me/")
+CREATE_USER_ENDPOINT = "marketplace/actors/backup/init"
+PUBLIC_KEY_ENDPOINT = "marketplace/actors/backup/public_key"
+REPO_NAMES = ["config_backup_1", "config_backup_2"]
+class Backup(BaseActor):
+    def repos_exist(self):
+        restic_repos = j.tools.restic.list_all()
+        for repo in REPO_NAMES:
+            if repo not in REPO_NAMES:
+                return False
+        return True
+    @actor_method
+    def init(self, password) -> str:
+        headers = {'Content-Type': 'application/json'}
+        tname = j.core.identity.me.tname
+        username = j.core.identity.me.tname.split(".")[0]
+        url = os.path.join(MARKETPLACE_URL, PUBLIC_KEY_ENDPOINT)
+        response = requests.post(url)
+        if response.status_code != 200:
+            raise Exception("Can not get market place publickey")
+        mrkt_pub_key  = nacl.public.PublicKey(response.json().encode(), nacl.encoding.Base64Encoder) 
+        priv_key = j.core.identity.me.nacl.private_key
+        box = Box(priv_key, mrkt_pub_key)
+        password_encrypted = box.encrypt(password.encode(), encoder=nacl.encoding.Base64Encoder).decode()
+        data = {
+            "threebot_name": tname,
+            "passwd": password_encrypted
+            }
+        url = os.path.join(MARKETPLACE_URL, CREATE_USER_ENDPOINT)
+        response = requests.post(url, json= data, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"can not create user with name:{username}, error={response.text}")
+        config_dict = dict(zip(REPO_NAMES, response.json()))
+        for repo_name, serverip in config_dict.items():
+            print(repo_name, serverip)
+            repo = j.tools.restic.get(repo_name)
+            repo.repo = f"rest:http://{username}:{password}@{serverip}:8000/{username}/"
+            repo.password = password
+            repo.init_repo()
+            repo.save()
+        return j.data.serializers.json.dumps({"data": "backup repos inited"})
+
+    @actor_method
+    def backup(self, tags = None) -> str:
+        if not self.repos_exist():
+            raise Exception("repos are not inited, please call init first")
+        tags = tags or []
+        for repo_name in REPO_NAMES:
+            repo = j.tools.restic.get(repo_name)
+            repo.backup(j.core.dirs.JSCFGDIR, tags=tags)
+        return j.data.serializers.json.dumps({"data": "backup done"})
+
+    
+    def get_last_snapshot(self, tags=None):
+        def _add_snapshot_timestamp(snapshot):
+            import arrow
+            snapshot["time"] = arrow.get(snapshot["time"]).timestamp
+            return snapshot
+
+        snapshots = []
+        for repo_name in REPO_NAMES:
+            repo = j.tools.restic.get(repo_name)
+            repo_snapshots = repo.list_snapshots(last=True, tags=tags, path=j.core.dirs.JSCFGDIR)
+            if repo_snapshots:
+                snapshot = repo_snapshots[0]
+                snapshot['repo_name'] = repo_name
+                snapshots.append(snapshot)
+        snapshots = list(map(_add_snapshot_timestamp, snapshots))
+        if not snapshots:
+            raise Exception("can not find any snapshots in the repo")
+        return max(snapshots, key=lambda x: x["time"])
+
+    @actor_method
+    def restore(self, tags=None) -> str:
+        if not self.repos_exist():
+            raise Exception("repos are not inited, please call init first")
+        snapshot = self.get_last_snapshot(tags=tags)
+        repo = j.tools.restic.get(snapshot['repo_name'])
+        repo.restore("/", snapshot_id=snapshot['id'])
+        return j.data.serializers.json.dumps({"data": "repos restored"})
+
+    @actor_method
+    def enable_auto_backup(self) -> str:
+        if not self.repos_exist():
+            raise Exception("repos are not inited, please call init first")
+        for repo_name in REPO_NAMES:
+            repo = j.tools.restic.get(repo_name)
+            repo.auto_backup(j.core.dirs.JSCFGDIR)
+        return j.data.serializers.json.dumps({"data": "auto backup enabled"})
+
+    @actor_method
+    def disable_auto_backup(self) -> str:
+        if not self.repos_exist():
+            raise Exception("repos are not inited, please call init first")
+        for repo_name in REPO_NAMES:
+            repo = j.tools.restic.get(repo_name)
+            repo.disable_auto_backup(j.core.dirs.JSJSCFGDIR)
+        return j.data.serializers.json.dumps({"data": "auto backup disabled"})
+        
+
+Actor = Backup
