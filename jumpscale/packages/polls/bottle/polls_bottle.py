b4ad3d6db365042bd2e4ed2082c00f0648a4d2e7
Waleed <waleed.hammam@gmail.com>
2020-07-22 18:59:49 +0200

    add endpoints for polls names

---
+++ jumpscale/packages/polls/bottle/polls_bottle.py
@@ -0,0 +1,52 @@
+import csv
+
+from beaker.middleware import SessionMiddleware
+from bottle import Bottle, static_file
+
+from jumpscale.loader import j
+from jumpscale.packages.auth.bottle.auth import SESSION_OPTS, login_required, admin_only
+from jumpscale.packages.polls.chats.threefold import VOTES
+from jumpscale.sals.chatflows.polls import all_users
+
+app = Bottle()
+
+templates_path = j.sals.fs.join_paths(j.sals.fs.dirname(__file__), "templates")
+env = j.tools.jinja2.get_env(templates_path)
+
+
+def _map_vote_results(user_votes, votes_questions):
+    votes_data = {}
+    for question_name, answers in user_votes.items():
+        for question in votes_questions.values():
+            if question["title"] == question_name:
+                answer = answers.index(1)
+                votes_data[question_name] = question["options"][answer]
+    return votes_data
+
+
+@app.route("/api/results")
+@login_required
+@admin_only
+def results():
+    votes_data = {}
+    for voter_name in all_users.list_all():
+        voter = all_users.get(voter_name)
+        votes_data[j.data.text.removeprefix(voter_name, "threefold_")] = _map_vote_results(voter.vote_data, VOTES)
+
+    votes_data["-Number of voters"] = all_users.count
+    return votes_data
+
+
+@app.route("/api/names")
+@login_required
+@admin_only
+def names():
+    data = {"names": []}
+    for voter_name in all_users.list_all():
+        voter = all_users.get(voter_name)
+        data["names"].append(voter.extra_data["full_name"])
+    data["-Number of voters"] = all_users.count
+    return data
+
+
+app = SessionMiddleware(app, SESSION_OPTS)
