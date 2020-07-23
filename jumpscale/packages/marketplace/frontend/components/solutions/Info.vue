52f7c34d2e8d10879eeb04f4749802a1329e7db7
Waleed <waleed.hammam@gmail.com>
2020-07-23 02:08:44 +0200

    change cancel to delete reservation

--- jumpscale/packages/marketplace/frontend/components/solutions/Info.vue
+++ jumpscale/packages/marketplace/frontend/components/solutions/Info.vue
@@ -3,10 +3,10 @@
     <template #default>
       <code-area mode="python" :content="json"></code-area>
       <!-- <pre>
-      </pre> -->
+      </pre>-->
     </template>
     <template #actions>
-      <v-btn text color="error" @click.stop="cancel(data.solution_type, data.id)">Cancel</v-btn>
+      <v-btn text color="error" @click.stop="cancel(data.solution_type, data.id)">Delete Reservation</v-btn>
       <v-btn text @click="close">Close</v-btn>
     </template>
   </base-dialog>
@@ -17,18 +17,21 @@ module.exports = {
   props: { data: Object },
   mixins: [dialog],
   computed: {
-    json () {
-      return JSON.stringify(JSON.parse(this.data.reservation), null, 2)
+    json() {
+      return JSON.stringify(JSON.parse(this.data.reservation), null, 2);
     }
   },
   methods: {
     cancel(solutionType, solutionId) {
-      this.$api.solutions.cancelReservation(solutionType, solutionId).then(response => {
-        console.log("cancel")
-        this.$router.go(0);
-      }).catch(err => {
-        console.log("failed")
-      });
+      this.$api.solutions
+        .cancelReservation(solutionType, solutionId)
+        .then(response => {
+          console.log("cancel");
+          this.$router.go(0);
+        })
+        .catch(err => {
+          console.log("failed");
+        });
     }
   }
 };
