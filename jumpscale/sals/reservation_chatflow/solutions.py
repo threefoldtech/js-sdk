031b2df61d78ce18f1508ca4431c045acb48d63e
m-motawea <magedmotawea@gmail.com>
2020-07-26 00:14:30 +0200

    ubuntu listing

--- jumpscale/sals/reservation_chatflow/solutions.py
+++ jumpscale/sals/reservation_chatflow/solutions.py
@@ -20,5 +20,61 @@ class ChatflowSolutions:
             )
         return result
 
+    def list_ubuntu_solutions(self, next_action=NextAction.DEPLOY, sync=True):
+        if sync:
+            j.sals.reservation_chatflow.deployer.load_user_workloads(next_action=next_action)
+        if not sync and not j.sals.reservation_chatflow.deployer.workloads[next_action][Type.Container]:
+            j.sals.reservation_chatflow.deployer.load_user_workloads(next_action=next_action)
+        result = []
+        for container_workloads in j.sals.reservation_chatflow.deployer.workloads[next_action][Type.Container].values():
+            for workload in container_workloads:
+                if not workload.info.metadata:
+                    continue
+                try:
+                    metadata = j.data.serializers.json.loads(workload.info.metadata)
+                except:
+                    continue
+                if not metadata.get("form_info"):
+                    continue
+                if metadata["form_info"].get("chatflow") == "ubuntu":
+                    result.append(
+                        {
+                            "wids": [workload.id],
+                            "Name": metadata.get("name", metadata["form_info"].get("Solution name")),
+                            "IP Address": workload.network_connection[0].ipaddress,
+                            "Network": workload.network_connection[0].network_id,
+                            "Node": workload.info.node_id,
+                            "Pool": workload.info.pool_id,
+                        }
+                    )
+        return result
+
+    def cancel_solution(self, solution_wids):
+        workload = j.sals.zos.workloads.get(solution_wids[0])
+        solution_uuid = self.get_solution_uuid(workload)
+        ids_to_delete = []
+        if solution_uuid:
+            # solutions created by new chatflows
+            for workload in j.sals.zos.workloads.list(j.me.tid, next_action="DEPLOY"):
+                if solution_uuid == self.get_solution_uuid(workload):
+                    ids_to_delete.append(workload.id)
+        else:
+            ids_to_delete = solution_wids
+
+        for wid in ids_to_delete:
+            j.sals.zos.workloads.decomission(wid)
+
+    def get_solution_uuid(self, workload):
+        if workload.info.metadata:
+            try:
+                metadata = j.data.serializers.json.loads(
+                    j.sals.chatflow_deployer.decrypt_metadata(workload.info.metadata)
+                )
+            except:
+                return
+            if metadata:
+                solution_uuid = metadata.get("solution_uuid")
+                return solution_uuid
+
 
 solutions = ChatflowSolutions()
