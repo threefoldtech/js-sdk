ab80d2a1f6b47eae7b1388ca649636eff0ee9f8b
dmahmouali <dmahmou213@gmail.com>
2020-07-28 20:15:54 +0200

    update solution and pool details view

--- jumpscale/packages/admin/frontend/components/solutions/Info.vue
+++ jumpscale/packages/admin/frontend/components/solutions/Info.vue
@@ -2,10 +2,42 @@
   <div>
     <base-dialog :title="title" v-model="dialog" :loading="loading">
       <template #default>
-        <code-area mode="python" :content="json"></code-area>
+        <v-list-item v-for="(item, key)  in json" :key="key">
+          <v-list-item-content v-if="key === 'nodes'">
+            <v-list-item-title v-text="key"></v-list-item-title>
+            <v-list-item v-for="(node, key)  in item" :key="key">
+              <v-list-item-title v-text="node"></v-list-item-title>
+              <v-list-item-subtitle v-text="key"></v-list-item-subtitle>
+            </v-list-item>
+          </v-list-item-content>
+          <v-list-item-content v-else-if="key === 'node_ids' ||key === 'active_workload_ids'">
+            <v-list-group :key="key" no-action>
+              <v-list-tile slot="activator">
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ key }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile v-for="node in item" :key="node">
+                <v-list-tile-content>
+                  <v-list-item-subtitle v-text="`- ${node}`"></v-list-item-subtitle>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list-group>
+          </v-list-item-content>
+          <v-list-item-content v-else>
+            <v-list-item-title v-text="key"></v-list-item-title>
+            <v-list-item-subtitle v-text="item"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <!-- <code-area mode="python" :content="json"></code-area> -->
       </template>
       <template #actions>
-        <v-btn v-if="data.Name !== undefined" text color="error" @click.stop="cancel()">Delete Reservation</v-btn>
+        <v-btn
+          v-if="data.Name !== undefined"
+          text
+          color="error"
+          @click.stop="cancel()"
+        >Delete Reservation</v-btn>
         <v-btn text @click="close">Close</v-btn>
       </template>
     </base-dialog>
@@ -29,7 +61,12 @@ module.exports = {
   },
   computed: {
     json() {
-      return JSON.stringify(this.data, null, 2);
+      console.log(this.data);
+      if (this.data.last_updated !== undefined)
+        this.data.last_updated = new Date(this.data.last_updated * 1000);
+      if (this.data.empty_at !== undefined)
+        this.data.empty_at = new Date(this.data.empty_at * 1000);
+      return this.data;
     },
     title() {
       return this.data.Name === undefined
