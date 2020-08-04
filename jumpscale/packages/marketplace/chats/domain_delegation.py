ae7b4a83b7f69a76ed42ed08a29278c86046ae93
m-motawea <magedmotawea@gmail.com>
2020-08-04 11:30:05 +0200

    marketplace domain delegation

---
+++ jumpscale/packages/marketplace/chats/domain_delegation.py
@@ -0,0 +1,17 @@
+from jumpscale.sals.chatflows.chatflows import chatflow_step
+from jumpscale.sals.marketplace import deployer, MarketPlaceChatflow
+from jumpscale.packages.tfgrid_solutions.chats.domain_delegation import DomainDelegation as BaseDomainDelegation
+import uuid
+
+
+class DomainDelegation(BaseDomainDelegation, MarketPlaceChatflow):
+    @chatflow_step(title="Pool")
+    def select_pool(self):
+        self._validate_user()
+        self.solution_id = uuid.uuid4().hex
+        self.solution_metadata = {"owner": self.user_info()["username"]}
+        self.gateway, pool = deployer.select_gateway(self.solution_metadata["owner"], bot=self)
+        self.pool_id = pool.pool_id
+
+
+chat = DomainDelegation
