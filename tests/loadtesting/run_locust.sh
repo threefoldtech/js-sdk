2db70e2d3d02aafef95366392b9b68c0d4446f46
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-08-06 17:25:23 +0200

    update the code to directly use the host and separate the gedis endpoint

--- tests/loadtesting/run_locust.sh
+++ tests/loadtesting/run_locust.sh
@@ -6,17 +6,15 @@ helpFunction() {
     echo -e "\t-r The rate per second in which users are spawned"
     echo -e "\t-h Host to load test"
     echo -e "\t-p Path for python script which uses locust in load testing"
-    echo -e "\t-n Number of port used in the request"
     exit 1 # Exit script after printing help
 }
 
-while getopts "u:r:h:p:n:" opt; do
+while getopts "u:r:h:p:" opt; do
     case "$opt" in
     u) user_number="$OPTARG" ;;
     r) hatch_rate="$OPTARG" ;;
     h) host="$OPTARG" ;;
     p) path="$OPTARG" ;;
-    n) port_number="$OPTARG" ;;
     ?) helpFunction ;; # Print helpFunction in case parameter is non-existent
     esac
 done
@@ -32,14 +30,6 @@ echo "$user_number"
 echo "$hatch_rate"
 echo "$host"
 echo "$path"
-echo "$port_number"
-
-# If the port number is not specified, we will use the default port 8000
-if [ -z "$port_number" ]; then
-    export PORTNO="8000"
-else
-    export PORTNO=$port_number
-fi
 
 # Call locust to do the load testing
 locust --headless -u $user_number -r $hatch_rate --host $host -f $path
