e8e044a3fc77fd6e605281991b4bd39c8376d309
m-motawea <magedmotawea@gmail.com>
2020-08-10 11:07:25 +0200

    expiration warning

--- jumpscale/packages/admin/frontend/components/dashboard/Pools.vue
+++ jumpscale/packages/admin/frontend/components/dashboard/Pools.vue
@@ -8,6 +8,9 @@
           <template v-slot:item.active_workload_ids="{ item }">
             {{ item.active_workload_ids.length }}
           </template>
+          <template v-slot:item.empty_at="{ item }">
+            <div :class="`${item.class}`">{{ item.empty_at }}</div>
+          </template>
       </v-data-table>
     </base-section>
     <pool-info v-model="dialog" :pool="selected"></pool-info>
@@ -35,12 +38,24 @@
     methods: {
       getPools () {
         this.loading = true
+        let today = new Date();
+        let alert_time = new Date()
+        alert_time.setDate(today.getDate() + 2)
         this.$api.solutions.getPools().then((response) => {
           this.pools = JSON.parse(response.data).data
           for (let i = 0; i < this.pools.length; i++) {
             pool = this.pools[i];
             if (pool.empty_at < 9223372036854775807) {
-              pool.empty_at = new Date(pool.empty_at * 1000).toLocaleString();
+              let pool_expiration = new Date(pool.empty_at * 1000);
+              pool.empty_at = pool_expiration.toLocaleString();
+              if (pool_expiration < today){
+                pool.class = "red--text";
+                pool.empty_at = "EXPIRED"
+              } else if (pool_expiration < alert_time && pool_expiration > today) {
+                pool.class = "red--text";
+              } else {
+                pool.class = "";
+              }
             } else {
               pool.empty_at = "-";
             }
