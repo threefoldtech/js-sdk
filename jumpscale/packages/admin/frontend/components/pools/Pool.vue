9b0554e0fc366aa7bb110e74b6e31305c2e5e39f
dmahmouali <dmahmou213@gmail.com>
2020-08-10 18:39:40 +0200

    added rename pool in dashboard

--- jumpscale/packages/admin/frontend/components/pools/Pool.vue
+++ jumpscale/packages/admin/frontend/components/pools/Pool.vue
@@ -10,7 +10,32 @@
             </tr>
             <tr>
               <td>Name</td>
-              <td>{{ pool.name }}</td>
+              <td>
+                <div class="justify-center">
+                  <v-row>
+                    <div style="margin-top: 15px;">{{ pool.name }}</div>
+                    <v-spacer></v-spacer>
+                    <v-text-field
+                      v-if="edting"
+                      v-model="name"
+                      label="New name"
+                      hint
+                      persistent-hint
+                    ></v-text-field>
+                    <v-btn v-if="!edting" small color="warning" @click="startEdit()" dark>Edit</v-btn>
+                    <div style="margin-top: 20px; margin-left: 10px;">
+                      <v-btn v-if="edting" small color="error" @click="cancelEdit()" dark>close</v-btn>
+                      <v-btn
+                        v-if="edting"
+                        small
+                        color="success"
+                        @click="rename(pool.pool_id, name)"
+                        dark
+                      >save</v-btn>
+                    </div>
+                  </v-row>
+                </div>
+              </td>
             </tr>
             <tr>
               <td>Farm</td>
@@ -40,16 +65,18 @@
               <td>Nodes</td>
               <td class="pt-2">
                 <v-chip class="ma-1" v-for="(node_id, index) in pool.node_ids" :key="index">
-                    <a :href="`${pool.explorer_url}/nodes/${node_id}`">{{ node_id }}</a>
+                  <a :href="`${pool.explorer_url}/nodes/${node_id}`">{{ node_id }}</a>
                 </v-chip>
               </td>
             </tr>
             <tr>
               <td>Active Workloads</td>
               <td class="pt-2">
-                <v-chip class="ma-1" v-for="(wid, index) in pool.active_workload_ids" :key="index">
-                  {{ wid }}
-                </v-chip>
+                <v-chip
+                  class="ma-1"
+                  v-for="(wid, index) in pool.active_workload_ids"
+                  :key="index"
+                >{{ wid }}</v-chip>
               </td>
             </tr>
           </tbody>
@@ -57,26 +84,39 @@
       </v-simple-table>
     </template>
     <template #actions>
+      <v-btn text color="error" @click="hide(pool.pool_id)">Hide</v-btn>
       <v-btn text @click="close">Close</v-btn>
-      <v-btn text @click="hide(pool.pool_id)">Hide</v-btn>
     </template>
   </base-dialog>
 </template>
 
 <script>
-
 module.exports = {
-  props: {pool: Object},
+  data() {
+    return {
+      edting: false,
+      name: "",
+    };
+  },
+  props: { pool: Object },
   mixins: [dialog],
   methods: {
-    rename (pool_id, name) {
-      this.$api.solutions.renamePool(pool_id, name)
-      window.location.reload()
+    startEdit() {
+      console.log(this.edting);
+      this.edting = true;
     },
-    hide (pool_id) {
-      this.$api.solutions.hidePool(pool_id)
-      window.location.reload()
+    cancelEdit() {
+      this.name = "";
+      this.edting = false;
     },
-  }
-}
+    rename(pool_id, name) {
+      this.$api.solutions.renamePool(pool_id, name);
+      window.location.reload();
+    },
+    hide(pool_id) {
+      this.$api.solutions.hidePool(pool_id);
+      window.location.reload();
+    },
+  },
+};
 </script>
