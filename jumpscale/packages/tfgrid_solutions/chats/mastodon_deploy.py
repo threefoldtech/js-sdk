f75eae0c51334ac13d626e238693e5eb971d32af
ranatrk <rana.t.hassan@gmail.com>
2020-08-11 19:17:42 +0200

    Simplify user questions in mastodon deploy chatflow

--- jumpscale/packages/tfgrid_solutions/chats/mastodon_deploy.py
+++ jumpscale/packages/tfgrid_solutions/chats/mastodon_deploy.py
@@ -1,11 +1,12 @@
 import math
+import random
+import uuid
 
 from jumpscale.clients.explorer.models import DiskType
 from jumpscale.loader import j
-from jumpscale.sals.chatflows.chatflows import GedisChatBot, chatflow_step, StopChatFlow
-from jumpscale.sals.reservation_chatflow.models import SolutionType
-import uuid
+from jumpscale.sals.chatflows.chatflows import GedisChatBot, StopChatFlow, chatflow_step
 from jumpscale.sals.reservation_chatflow import deployer, solutions
+from jumpscale.sals.reservation_chatflow.models import SolutionType
 
 
 class MastodonDeploy(GedisChatBot):
@@ -14,16 +15,12 @@ class MastodonDeploy(GedisChatBot):
     steps = [
         "mastodon_start",
         "mastodon_name",
-        "upload_public_key",
-        "database_credentials",
         "smtp_credentials",
         "select_pool",
         "mastodon_network",
         "container_node_id",
-        "container_ip",
-        "domain_select",
-        "ipv6_config",
         "overview",
+        "subdomain_select",
         "reservation",
         "mastodon_access",
     ]
@@ -35,7 +32,7 @@ class MastodonDeploy(GedisChatBot):
         self.solution_id = uuid.uuid4().hex
         self.md_show("# This wizard will help you deploy a mastodon container", md=True)
         self.solution_metadata = {}
-        self.user_info = self.user_info()
+        self.user_info_dict = self.user_info()
 
     @chatflow_step(title="Solution name")
     def mastodon_name(self):
@@ -51,20 +48,6 @@ class MastodonDeploy(GedisChatBot):
                     break
                 valid = True
 
-    @chatflow_step(title="Access key")
-    def upload_public_key(self):
-        self.public_key = self.upload_file(
-            "Please upload your public ssh key, this will allow you to access your threebot container using ssh"
-        ).strip()
-
-    @chatflow_step(title="Database credentials")
-    def database_credentials(self):
-        # self.domain = self.string_ask("Please add domain/url", default="64.227.1.81", required=True)
-        self.db_user = self.string_ask("Please add a username for the database", default="mastodon", required=True)
-        self.db_name = self.string_ask(
-            "Please add a name for the database", default="mastodon_production", required=True
-        )
-
     @chatflow_step(title="SMTP server credentials")
     def smtp_credentials(self):
         # self.domain = self.string_ask("Please add domain/url", default="64.227.1.81", required=True)
@@ -73,20 +56,20 @@ class MastodonDeploy(GedisChatBot):
         smtp_port = form.string_ask("Please add the port the SMTP server exposes", default="587", required=True)
         smtp_login = form.string_ask("Please add the SMTP login name", default="apikey", required=True)
         smtp_password = form.string_ask("Please add the SMTP login password", required=True)
-        smtp_from_address = form.string_ask("Please add address emails will be sent from", required=True)
+        smtp_from_address = form.string_ask("Please add the email address mastodon will be sending from", required=True)
         form.ask()
 
         self.env = {
             # "DOMAIN": "64.227.1.81",  # TODO create subdomain
-            "DB_USER": self.db_user,
-            "DB_NAME": self.db_name,
+            "DB_USER": self.user_info_dict["username"],
+            "DB_NAME": "mastodon_" + self.user_info_dict["username"],
             "SMTP_SERVER": smtp_server.value,
             "SMTP_PORT": smtp_port.value,
             "SMTP_LOGIN": smtp_login.value,
-            "SMTP_PASSWORD": smtp_password.value,
             "SMTP_FROM_ADDRESS": smtp_from_address.value,
-            "pub_key": self.public_key or "",
+            "pub_key": "",
         }
+        self.secret_env = {"SMTP_PASSWORD": smtp_password.value}
 
     @chatflow_step(title="Pool")
     def select_pool(self):
@@ -105,8 +88,23 @@ class MastodonDeploy(GedisChatBot):
         if not self.selected_node:
             self.selected_node = deployer.schedule_container(self.pool_id, **query)
 
-    @chatflow_step(title="Container IP")
-    def container_ip(self):
+    @chatflow_step(title="Confirmation")
+    def overview(self):
+        self.metadata = {
+            "Solution Name": self.solution_name,
+            "Network": self.network_view.name,
+            "Node ID": self.selected_node.node_id,
+            "Pool": self.pool_id,
+            "SMTP Server name": self.env["SMTP_SERVER"],
+            "SMTP port": self.env["SMTP_PORT"],
+            "SMTP login user": self.env["SMTP_LOGIN"],
+            "SMTP address to send from": self.env["SMTP_FROM_ADDRESS"],
+        }
+        self.md_show_confirm(self.metadata)
+
+    @chatflow_step(title="Domain selection")
+    def subdomain_select(self):
+        # select ip address
         self.network_view_copy = self.network_view.copy()
         result = deployer.add_network_node(
             self.network_view.name,
@@ -124,12 +122,9 @@ class MastodonDeploy(GedisChatBot):
                     raise StopChatFlow(f"Failed to add node {self.selected_node.node_id} to network {wid}")
             self.network_view_copy = self.network_view_copy.copy()
         free_ips = self.network_view_copy.get_node_free_ips(self.selected_node)
-        self.ip_address = self.drop_down_choice(
-            "Please choose IP Address for your solution", free_ips, default=free_ips[0], required=True
-        )
+        self.ip_address = random.choice(free_ips)
 
-    @chatflow_step(title="Domain")
-    def domain_select(self):
+        # select domain
         gateways = deployer.list_all_gateways()
         if not gateways:
             raise StopChatFlow("There are no available gateways in the farms bound to your pools.")
@@ -140,13 +135,16 @@ class MastodonDeploy(GedisChatBot):
             for domain in gateway.managed_domains:
                 domains[domain] = gw_dict
 
-        self.domain = self.single_choice(
-            "Please choose the domain you wish to use", list(domains.keys()), required=True
-        )
+        self.domain = random.choice(list(domains.keys()))
+        # self.single_choice("Please choose the domain you wish to use", list(domains.keys()), required=True)
 
         self.gateway = domains[self.domain]["gateway"]
         self.gateway_pool = domains[self.domain]["pool"]
-        self.domain = f"{self.threebot_name}-{self.solution_name}.{self.domain}"
+        self.subdomain = self.string_ask(
+            "Please specify a subdomain that you will use to access mastodon from the browser. It will be in the form `<YOUR_SUBDOMAIN>.{self.domain}`",
+            required=True,
+        )
+        self.domain = f"{self.subdomain}.{self.domain}"
 
         self.addresses = []
         for ns in self.gateway.dns_nameserver:
@@ -154,29 +152,16 @@ class MastodonDeploy(GedisChatBot):
 
         self.secret = f"{j.core.identity.me.tid}:{uuid.uuid4().hex}"
 
-    @chatflow_step(title="Global IPv6 Address")
-    def ipv6_config(self):
-        self.public_ipv6 = deployer.ask_ipv6(self)
-
-    @chatflow_step(title="Confirmation")
-    def overview(self):
-        self.metadata = {
-            "Solution Name": self.solution_name,
-            "Network": self.network_view.name,
-            "Node ID": self.selected_node.node_id,
-            "Pool": self.pool_id,
-            "CPU": 1,
-            "Memory": 1,
-            "Disk Size": 10,
-            "IP Address": self.ip_address,
-            "Database User": self.env["DB_USER"],
-            "Database name": self.env["DB_NAME"],
-            "SMTP Server name": self.env["SMTP_SERVER"],
-            "SMTP port": self.env["SMTP_PORT"],
-            "SMTP login user": self.env["SMTP_LOGIN"],
-            "SMTP address to send from": self.env["SMTP_FROM_ADDRESS"],
-        }
-        self.md_show_confirm(self.metadata)
+        self.metadata.update(
+            {
+                "IP Address": self.ip_address,
+                "CPU": 1,
+                "Memory": 1,
+                "Disk Size": 10,
+                "Database User": self.env["DB_USER"],
+                "Database name": self.env["DB_NAME"],
+            }
+        )
 
     @chatflow_step(title="Reservation")
     def reservation(self):
@@ -191,7 +176,7 @@ class MastodonDeploy(GedisChatBot):
                 solution_uuid=self.solution_id,
                 **self.solution_metadata,
             )
-            success = deployer.wait_workload(self.c, self)
+            success = deployer.wait_workload(self.subdomain_workload_id, self)
             if not success:
                 raise StopChatFlow(
                     f"Failed to create subdomain {self.domain} on gateway {self.gateway.node_id} {self.subdomain_workload_id}"
@@ -216,8 +201,9 @@ class MastodonDeploy(GedisChatBot):
             disk_size=10,
             interactive=True,  # TODO change to False
             env=self.env,
+            secret_env=self.secret_env,
             entrypoint="/start_mastodon.sh",
-            public_ipv6=self.public_ipv6,
+            # public_ipv6=self.public_ipv6,
             **self.solution_metadata,
             solution_uuid=self.solution_id,
         )
@@ -232,7 +218,7 @@ class MastodonDeploy(GedisChatBot):
             network_name=self.network_view.name,
             trc_secret=self.secret,
             domain=self.domain,
-            email=self.user_info["email"],
+            email=self.user_info_dict["email"],
             solution_ip=self.ip_address,
             solution_port=443,
             enforce_https=False,
