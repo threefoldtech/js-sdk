55682bd9bcdf0d46cac1dc7b2cb483b878331853
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-08-13 13:10:22 +0200

    add internal packages by name

--- jumpscale/packages/admin/frontend/components/external/Notebooks.vue
+++ jumpscale/packages/admin/frontend/components/external/Notebooks.vue
@@ -8,7 +8,6 @@
       return {
         package: true,
         name: "notebooks",
-        giturl: "https://github.com/threefoldtech/js-sdk/blob/development/jumpscale/packages/notebooks",
         url: "/notebooks/"
       }
     }
