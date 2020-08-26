c2d4fc0ab4c5380d775715ebfe7dc1bf4b434041
m-motawea <magedmotawea@gmail.com>
2020-08-26 20:08:36 +0200

    simplified threebot

--- jumpscale/packages/marketplace/frontend/data.js
+++ jumpscale/packages/marketplace/frontend/data.js
@@ -37,12 +37,6 @@ const SOLUTIONS = {
     image: "./assets/network.png",
     description: "Deploy a network on the grid to connect your solutions together."
   },
-  threebot: {
-    name: "3Bot",
-    type: "threebot",
-    image: "./assets/3bot.png",
-    description: "Deploy your 3Bot on container."
-  },
   expose: {
     name: "Solution Expose",
     type: "exposed",
@@ -77,6 +71,12 @@ const SOLUTIONS = {
 
 
 const APPS = {
+  threebot: {
+    name: "3Bot",
+    type: "threebot",
+    image: "./assets/3bot.png",
+    description: "Deploy your 3Bot on container."
+  },
   wiki: {
     name: "Wiki",
     type: "wiki",
