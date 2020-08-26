5868a00456b437165900ee8bada68e0e13f0c0ce
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2020-08-27 01:44:10 +0200

    Adds backup task
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- jumpscale/sals/marketplace/backup_volume.sh
+++ jumpscale/sals/marketplace/backup_volume.sh
@@ -2,14 +2,19 @@
 
 # TODO: Add to the docker file
 apt-get update;
-apt-get install restic;
+apt-get install -y restic crontab ;
 
-restic init;
-
-for path in ${BACKUP_PATHS[*]}
-do
-  restic backup $path;
-done
-
-# To restore:
-# restic --target TARGET_PATH restore latest
+if restic list snapshots; then
+  if [ -z `restic snapshots --json` ]; then
+    restic init; 
+  else 
+      for path in ${BACKUP_PATHS[*]}
+      do
+        restic --target $path retore latest;
+      done 
+    fi
+else
+    restic init;
+fi
+echo "for path in ${BACKUP_PATHS[*]}; do restic backup $path; done" > /root/backup.sh
+echo "0 0 * * *  root bash /root/backup.sh" >> /etc/cron.d/backup
