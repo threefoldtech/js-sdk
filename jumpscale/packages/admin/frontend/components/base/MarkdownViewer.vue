138654aacf053a8f5bace22f40a02e2003306be5
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-08-30 19:52:30 +0200

    admin: add links to legal documents in header and render them directly

---
+++ jumpscale/packages/admin/frontend/components/base/MarkdownViewer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-row v-if="loading" style="height:100%" align="center" justify="center">
+      <v-progress-circular size="100" width="5" color="primary" indeterminate></v-progress-circular>
+    </v-row>
+    <!-- <vue-simple-markdown v-else :source="source" :emoji="false"></vue-simple-markdown> -->
+    <div v-html="source"></div>
+  </v-container>
+</template>
+
+<script>
+module.exports = {
+  data() {
+    return {
+      source: "",
+      loading: true
+    };
+  },
+  props: {
+    url: String,
+    baseUrl: String
+  },
+  mounted() {
+    this.loading = true;
+    this.$root.$emit("sidebar", true);
+    this.$api.content.get(this.url).then(res => {
+      let options = {};
+      if (this.baseUrl) {
+        options.baseUrl = this.baseUrl;
+      }
+      this.source = marked(res.data, options);
+      this.loading = false;
+    });
+  }
+};
+</script>
