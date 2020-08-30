5f1109194221c91cd9941fa4518f856633a69161
AhmedSa-mir <asamir97@hotmail.com>
2020-08-30 17:17:38 +0200

    Changed checking for solutions to first step

--- jumpscale/packages/tfgrid_solutions/chats/exposed.py
+++ jumpscale/packages/tfgrid_solutions/chats/exposed.py
@@ -37,17 +37,26 @@ class SolutionExpose(GedisChatBot):
     @chatflow_step(title="Solution type")
     def solution_type(self):
         self._deployment_start()
-        self.kind = self.single_choice("Please choose the solution type", list(kinds.keys()), required=True)
-        solutions = kinds[self.kind]()
+
+        available_solutions = {}
+        for kind in list(kinds.keys()):
+            solutions = kinds[kind]()
+            if solutions:
+                available_solutions.update({kind: solutions})
+
+        if not available_solutions:
+            raise StopChatFlow(f"You don't have any solutions to expose")
+
+        self.kind = self.single_choice(
+            "Please choose the solution type", list(available_solutions.keys()), required=True
+        )
         self.sols = {}
-        for sol in solutions:
+        for sol in available_solutions[self.kind]:
             name = sol["Name"]
             self.sols[name] = sol
 
     @chatflow_step(title="Solution to be exposed")
     def exposed_solution(self):
-        if not self.sols:
-            raise StopChatFlow(f"You don't have {self.kind} solutions")
         self.solution_name = self.single_choice(
             "Please choose the solution to expose", list(self.sols.keys()), required=True
         )
