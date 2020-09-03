48b2602fac14e7c48239f4d0c449575b376d51f4
m-motawea <magedmotawea@gmail.com>
2020-09-03 19:51:58 +0200

    remove unused imports

--- jumpscale/packages/marketplace/chats/blog.py
+++ jumpscale/packages/marketplace/chats/blog.py
@@ -1,6 +1,3 @@
-import math
-from jumpscale.sals.marketplace import deployer
-
 from jumpscale.packages.marketplace.chats.publisher import Publisher
 from jumpscale.sals.chatflows.chatflows import chatflow_step
 
