125019553823b6ae4ba76c9530abc14be63a1ddf
m-motawea <magedmotawea@gmail.com>
2020-09-09 12:18:39 +0200

    fix show_payment

--- jumpscale/sals/reservation_chatflow/deployer.py
+++ jumpscale/sals/reservation_chatflow/deployer.py
@@ -372,30 +372,20 @@ class ChatflowDeployer:
         for w in wallets.keys():
             wallet_names.append(w)
         wallet_names.append("External Wallet (QR Code)")
-        thecurrency = escrow_asset.split(":")[0]
+        self.msg_payment_info, qr_code = self.get_qr_code_payment_info(pool)
         message = f"""
         <h3>Billing details:</h3><br>
-        <b> Wallet address:</b>  {escrow_address}<br>
-        <b> Currency: </b>  {thecurrency}<br>
-        <b> Total Amount: </b> {total_amount} {thecurrency}<br>
-        <b> Transaction Fees:</b> 0.1 {thecurrency}<br>
+        {self.msg_payment_info}
         <br><hr><br>
         <h3> Choose a wallet name to use for payment or proceed with payment through External wallet (QR Code) </h3>
         """
         result = bot.single_choice(message, wallet_names, html=True)
-        qr_code = f"{thecurrency}:{escrow_address}?amount={total_amount}&message=p-{resv_id}&sender=me"
         if result == "External Wallet (QR Code)":
             msg_text = f"""
             <h3>Make a Payment</h3>
             Scan the QR code with your wallet (do not change the message) or enter the information below manually and proceed with the payment. Make sure to put p-{resv_id} as memo_text value.
 
-            <h4> Destination Wallet Address: </h4>  {escrow_address} \n
-            <h4> Currency: </h4>  {thecurrency} \n
-            <h4> Total Amount: </h4> {total_amount} {thecurrency} \n
-            <h4> Memo Text (Reservation ID): </h4>  p-{resv_id} \n
-
-            <h5>Inserting the memo-text is an important way to identify a transaction recipient beyond a wallet address. Failure to do so will result in a failed payment. Please also keep in mind that an additional Transaction fee of 0.1 FreeTFT will automatically occurs per transaction.</h5>
-
+            {self.msg_payment_info}
 
             """
             bot.qrcode_show(data=qr_code, msg=msg_text, scale=4, update=True, html=True)
