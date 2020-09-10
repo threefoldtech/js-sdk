b8c0c3602aaf9c855ef36fddb22753d4b1bd3f19
Waleed <waleed.hammam@gmail.com>
2020-09-10 15:22:31 +0200

    Add creating funded test wallet option to stellar client

--- jumpscale/clients/stellar/__init__.py
+++ jumpscale/clients/stellar/__init__.py
@@ -1,3 +1,5 @@
+import requests
+
 from jumpscale.core.base import StoredFactory
 from stellar_sdk import Keypair
 from .exceptions import *
@@ -14,6 +16,42 @@ class StellarFactory(StoredFactory):
             instance.secret = secret
 
         return instance
+
+    def create_testwallet(self, name: str):
+        """This method will create a testnet wallet and fund it from the facuet
+
+        Args:
+            name (str): name of the wallet
+
+        Return:
+            wallet balance
+        """
+        wallet = self.new(name)
+        wallet.network = "TEST"
+        wallet.activate_through_friendbot()
+        wallet.add_known_trustline("TFT")
+        wallet.save()
+
+        headers = {"Content-Type": "application/json"}
+        url = "https://gettft.testnet.grid.tf/tft_faucet/api/transfer"
+        data = {"destination": wallet.address}
+
+        try:
+            response = requests.post(url, json=data, headers=headers)
+        except requests.exceptions.HTTPError:
+            j.logger.error(
+                f"Failed to fund wallet can't reach {url} due to connection error. Changes will be reverted."
+            )
+            self.delete(name)
+            return
+
+        if response.status_code != 200:
+            j.logger.error(f"Failed to fund the wallet due to to facuet server error. Changes will be reverted.")
+            self.delete(name)
+            return
+
+        j.logger.info("Wallet created successfully")
+        return wallet.get_balance()
 
 
 def export_module_as():
