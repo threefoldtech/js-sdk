887f31fd4edfd2ba22e76b45503a19e9ba571941
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-09-28 17:30:51 +0200

    update env var

--- tests/clients/digitalOcean/env_var.sh
+++ tests/clients/digitalOcean/env_var.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export SSH_PRIVATE_KEY_PATH="/home/rafy/.ssh/id_rsa"
-export DIGITAL_OCEAN_ACCESS_TOKEN="3ec203148d5f5dd59fafa86bd34f9ea6767f554d6392ecc4b22a81b98c1b5c67"
+export SSH_PRIVATE_KEY_PATH=""
+export DIGITAL_OCEAN_ACCESS_TOKEN=""
