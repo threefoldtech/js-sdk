96d790aaaecd8b27c190e8181530deea14010f5e
Mostafa Hassan <mhost39@gmail.com>
2020-09-29 16:21:20 +0200

    add test for github

--- tests/clients/github/test_githubclient.py
+++ tests/clients/github/test_githubclient.py
@@ -6,6 +6,7 @@
 # organization: fakeForTest2
 
 from unittest import TestCase
+import string
 
 from jumpscale.loader import j
 
@@ -25,8 +26,36 @@ class GithubClientTest(TestCase):
         self.client.username = self.username
         self.client.password = self.password
 
+    def tearDown(self):
+        j.clients.github.delete(self.instance_name)
+
+    def random_string(self):
+        return j.data.idgenerator.nfromchoices(10, string.ascii_lowercase)
+
+    def info(self, msg):
+        j.logger.info(msg)
+
     def test001_github_client_get_access(self):
         self.assertEqual(self.client.get_userdata()["emails"][0]["email"], self.email)
 
-    def tearDown(self):
-        j.clients.github.delete(self.instance_name)
+    def test002_github_create_repo(self):
+        """Test case for create repo.
+
+        **Test Scenario**
+        #. create repo.
+        #. Check if repo is created.
+        #. Delete this repo.
+        #. check that this repo is deleted.
+        """
+        self.info("create repo")
+        repo_name = self.random_string()
+        repo = self.client.create_repo(name=repo_name)
+
+        self.info("Check if repo is created")
+        self.assertIn(repo.full_name, str(self.client.get_repos()))
+
+        self.info("Delete this repo")
+        self.client.delete_repo(repo_name=repo.name)
+
+        self.info("check that this repo is deleted.")
+        self.assertNotIn(repo.full_name, str(self.client.get_repos()))
