7716bf58867e5bf37ff9e1a1c5bf5fdf27753c9b
ahmedhanafy725 <hanafya@codescalers.com>
2020-10-04 18:15:21 +0200

    Add frontend tests to github actions
    Setup identity before starting the tests

--- tests/frontend/tests/base_tests.py
+++ tests/frontend/tests/base_tests.py
@@ -1,3 +1,4 @@
+import os
 import time
 from unittest import TestCase
 from urllib.parse import urljoin
@@ -5,25 +6,66 @@ from urllib.parse import urljoin
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 
+from jumpscale.core.base import StoredFactory
 from jumpscale.loader import j
+from jumpscale.packages.admin.bottle.models import UserEntry
 from tests.frontend.pages.base import Base
 
 
 class BaseTest(TestCase):
     @classmethod
     def setUpClass(cls):
+        # Set auto login config to disable 3bot connect login.
         j.core.config.set("AUTO_LOGIN", True)
+
+        # Get environment variables to create identity.
+        cls.tname = os.environ.get("TNAME")
+        cls.email = os.environ.get("EMAIL")
+        cls.words = os.environ.get("WORDS")
+        cls.explorer_url = "https://explorer.testnet.grid.tf/api/v1"
+        if not (cls.tname and cls.email and cls.words):
+            raise Exception("Please add (TNAME, EMAIL, WORDS) of your 3bot identity as environment variables")
+
+        # Check if there is identity registered to set it back after the tests are finished.
+        cls.me = None
+        if j.core.identity.list_all() and hasattr(j.core.identity, "me"):
+            cls.me = j.core.identity.me
+
+        # Accept T&C for testing identity.
+        user_factory = StoredFactory(UserEntry)
+        user_entry = user_factory.get(f"{cls.tname.replace('.3bot', '')}")
+        user_entry.has_agreed = True
+        user_entry.tname = cls.tname
+        user_entry.save()
+
+        # Configure test identity and start threebot server.
+        cls.identity_name = j.data.random_names.random_name()
+        identity = j.core.identity.new(
+            cls.identity_name, tname=cls.tname, email=cls.email, words=cls.words, explorer_url=cls.explorer_url
+        )
+        identity.register()
+        j.core.identity.set_default(cls.identity_name)
         cls.server = j.servers.threebot.get("default")
         cls.server.start()
 
     @classmethod
     def tearDownClass(cls):
+        # Disable auto login after the tests are finished.
         j.core.config.set("AUTO_LOGIN", False)
+
+        # Stop threebot server and the testing identity.
         cls.server.stop()
+        j.core.identity.delete(cls.identity_name)
+
+        # Restore the user identity
+        if cls.me:
+            j.core.identity.set_default(cls.me.instance_name)
 
     def setUp(self):
+        # Configure chrome driver and go to the entrypoint.
         options = ChromeOptions()
         options.add_argument("--no-sandbox")
+        options.add_argument("--headless")
         options.add_argument("ignore-certificate-errors")
         self.driver = webdriver.Chrome(chrome_options=options)
         self.driver.implicitly_wait(10)
@@ -32,7 +74,8 @@ class BaseTest(TestCase):
         self.driver.get(urljoin(Base.base_url, self.login_endpoint))
 
     def tearDown(self):
-        if not self._outcome.success:
+        # Take screenshot for failure test.
+        if self._outcome.errors:
             self.driver.save_screenshot(f"{self._testMethodName}.png")
         self.driver.close()
 
