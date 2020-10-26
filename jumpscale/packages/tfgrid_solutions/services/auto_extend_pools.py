ada31e68c0a8ccf69a422e9d640fbb2513a683c9
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-10-26 17:17:15 +0200

    change the location of extending pools methods to be in tf-grid-soultion instead of threebot_deployer and adding mail service

---
+++ jumpscale/packages/tfgrid_solutions/services/auto_extend_pools.py
@@ -0,0 +1,17 @@
+from jumpscale.tools.servicemanager.servicemanager import BackgroundService
+from jumpscale.packages.tfgrid_solutions.utils import auto_extend_pools
+
+
+class AutoExtendPoolService(BackgroundService):
+    def __init__(self, name="threebot_deployer_auto_extend_pools", interval=60 * 60 * 24, *args, **kwargs):
+        """
+        Test service that runs every 1 day
+        """
+        super().__init__(name, interval, *args, **kwargs)
+
+    def job(self):
+        auto_extend_pools()
+        print("Auto extend pool service done successfully")
+
+
+service = AutoExtendPoolService()
