135d45087cac9ed55b95e3cecf1385f051acb4b7
AhmedSa-mir <asamir97@hotmail.com>
2020-10-26 13:24:02 +0200

    Added docs

--- jumpscale/tools/servicemanager/servicemanager.py
+++ jumpscale/tools/servicemanager/servicemanager.py
@@ -1,3 +1,42 @@
+"""
+### Description
+
+Service manager is the service that monitors and manages background services through gevent greenlets.
+Each service defines an interval to define the period of the service and defines also a job method that is run each period.
+Any service should:
+- Inherit from `BackgroundService` class defined here: `from jumpscale.tools.servicemanager.servicemanager import BackgroundService`
+- Define a name and interval in the constructor
+- Implement the abtsract `job` method of the `BackgroundService` base class.
+
+### How it schedules services
+
+The service manager uses gevent greenlets to run jobs. It spawns the job in a greenlet after its interval period.
+Rescheduling the service job is done in by linking a callback to the greenlet which is run after the greenlet finishes.
+After the greenlet finishes execution the callback is fired which schedules the job to be run again after anothre interval.
+
+To add a service  to the service manager you should call the `add_service` method which takes the package path as a parameter.
+It loads the file in this path as a module and gets the service object defined in the service.py file.
+
+### Example service
+
+```python3
+from jumpscale.tools.servicemanager.servicemanager import BackgroundService
+
+class TestService(BackgroundService):
+    def __init__(self, name="test", interval=20, *args, **kwargs):
+        '''
+            Test service that runs every 1 hour
+        '''
+        super().__init__(name, interval, *args, **kwargs)
+
+    def job(self):
+        print("[Test Service] Done")
+
+service = TestService()
+```
+"""
+
+
 from abc import ABC, abstractmethod
 import gevent
 from signal import SIGTERM, SIGKILL
