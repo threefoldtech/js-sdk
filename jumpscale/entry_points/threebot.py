14b908b0b93ddde6d230a55de5edb541e59f0089
Waleed <waleed.hammam@gmail.com>
2020-11-03 14:31:15 +0200

    add wallet according to the source of deployment in 3bot deployer

--- jumpscale/entry_points/threebot.py
+++ jumpscale/entry_points/threebot.py
@@ -274,9 +274,9 @@ def create_main_wallet(wallet_name):
 
 def create_wallets_if_not_exists():
     test, main = have_wallets()
-    if not test:
+    if not test and "testnet" in j.core.identity.me.explorer_url:
         create_test_wallet("test")
-    if not main:
+    if not main and "explorer.grid.tf" in j.core.identity.me.explorer_url:
         create_main_wallet("main")
 
 
