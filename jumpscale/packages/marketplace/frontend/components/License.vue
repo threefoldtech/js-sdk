3bae93b27c6933a081e5a3b53f4efcff3bb9f51d
xmonader <xmonader@gmail.com>
2020-11-04 14:08:10 +0200

    update marketplace info messages

--- jumpscale/packages/marketplace/frontend/components/License.vue
+++ jumpscale/packages/marketplace/frontend/components/License.vue
@@ -37,13 +37,12 @@
       <v-card>
         <v-card-title class="headline"> Welcome to ThreeFold Now </v-card-title>
         <v-card-text>
-          This is for demo purposes to showcase grid usage. We're running on
-          testnet which means it's always on latest testing code. Forgive any
-          instability you might encounter while our developers work out the
-          kinks. Your deployment will be automatically canceled in 3 hours.
+          This marketplace is a showcase of open source peer-to-peer apps built on top of the TF Grid. We are in demo mode
+          and running on testnet. Note your deployment will be cancelled automatically after three hours.
+          Forgive any instability you might encounter while our developers work out the kinks.
           <br />
           Please visit the
-          <a href="https://now.threefold.io/" target="_blank">manual</a> for
+          <a href="https://manual.threefold.io/" target="_blank">manual</a> and <a href="https://now.threefold.io/" target="_blank">wiki</a> for
           more information.
         </v-card-text>
         <v-card-actions>
