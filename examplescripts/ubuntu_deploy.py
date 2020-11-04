374f69d962864aafd31a2e9af385527106d0d60b
m-motawea <magedmotawea@gmail.com>
2020-11-04 15:16:35 +0200

    fix ubuntu_deploy

--- examplescripts/ubuntu_deploy.py
+++ examplescripts/ubuntu_deploy.py
@@ -11,7 +11,60 @@ Steps:
 from jumpscale.loader import j
 from time import sleep
 import netaddr
-import random
+from jumpscale.clients.explorer.models import NextAction, State, WorkloadType
+
+
+zos = j.sals.zos.get()
+WALLET_NAME = "test"
+network_name = "management"
+ip_range = "10.100.0.0/16"
+wg_quick = None
+
+
+bad_nodes = [
+    "A7FmQZ72h7FzjkJMGXmzLDFyfyxzitDZYuernGG97nv7",  # storage problem (not enough space in ssd pools)
+    "J1Xb2piba1vZM2pSmWP24CPgREnoTce7EuUJysttCyz6",
+    "A34YUGenHKyhjDMAUKZe4cVDtJM2wQ4n4XRkfGUUEYdy",
+    "7Qa7fxRPtMc5R72cnNms4XWD9PJkeQJ6iD8iTU9T8p2o",
+    "HYwvrxCy5z1QPALC5gTAiAMYWLH7orQ8xWEDbEyLiJV7",
+    "Dko5HMMaJ9D78rAfgobA3sE29FiVrKst64v2WM4Q52gx",
+    "9AcLUTkRRWGGQBn8T83Qkmuv1mwhjvmJmcvhS6r8Y8Kf",  # times out
+    "HEv8cqTakrSzoxeTyauBiRNomsMFB5N2iqCfHFDY63ct",  # times out
+    "J7twE8kmwkeph5WeKGcDoo1mUFgyYiWANFHtvE79L6Fp",  # times out -> about five minutes y3ne
+    "FVamMByotQxr5azHA9xLoo99qP7FNi1ukysMxpm6apNG",
+    "CBDY1Fu4CuxGpdU3zLL9QT5DGaRkxjpuJmzV6V5CBWg4",  # Bad access node
+]
+
+
+def get_free_ip(network, node_id, workloads=None):
+    workloads = workloads or zos.workloads.list(j.core.identity.me.tid)
+    used_ips = set()
+    for workload in workloads:
+        if workload.info.node_id != node_id:
+            continue
+        if workload.info.next_action != NextAction.DEPLOY:
+            continue
+        if workload.info.workload_type == WorkloadType.Kubernetes:
+            if workload.network_id == network.name:
+                used_ips.add(workload.ipaddress)
+        elif workload.info.workload_type == WorkloadType.Container:
+            for conn in workload.network_connection:
+                if conn.network_id == network.name:
+                    used_ips.add(conn.ipaddress)
+    node_range = None
+    for resource in network.network_resources:
+        if resource.info.node_id == node_id:
+            node_range = resource.iprange
+            break
+
+    hosts = netaddr.IPNetwork(node_range).iter_hosts()
+    next(hosts)  # skip ip used by node
+    for host in hosts:
+        ip = str(host)
+        if ip not in used_ips:
+            used_ips.add(ip)
+            return ip
+    return None
 
 
 def wait_workload(workload_id, expiry=10):
@@ -33,15 +86,6 @@ def wait_workload(workload_id, expiry=10):
     return False
 
 
-def get_node_free_ip(ip_range):
-    """
-    get a node free ip, skips the first ip as it is assigned to the network
-    """
-    hosts = netaddr.IPNetwork(ip_range).iter_hosts()
-    next(hosts)
-    return str(next(hosts))  # skip ip used by node
-
-
 def get_free_iprange(ip_range, network_used_ranges):
     network_range = netaddr.IPNetwork(ip_range)
     for _, subnet in enumerate(network_range.subnet(24)):
@@ -52,11 +96,10 @@ def get_free_iprange(ip_range, network_used_ranges):
         raise j.exceptions.Runtime(f"can't find free subnets in network {network_name}")
 
 
-zos = j.sals.zos.get()
-WALLET_NAME = "test"
-network_name = "management"
-ip_range = "10.100.0.0/16"
-wg_quick = None
+def get_deploymnet_node(pool):
+    available_nodes = list(zos.nodes_finder.nodes_by_capacity(cru=0, sru=0, mru=0, pool_id=pool.pool_id))
+    available_nodes = [node for node in available_nodes if node.node_id not in bad_nodes]
+    return available_nodes[0]
 
 
 payment_detail = zos.pools.create(cu=100, su=100, farm="freefarm", currencies=["TFT"])
@@ -113,26 +156,36 @@ for node in filter(zos.nodes_finder.filter_public_ip4, pool_nodes):
 else:
     raise Exception("can not find a node with ipv4 that can be used as access node for the cluster")
 
-freerange1 = get_free_iprange(ip_range, network_used_ranges)
-zos.network.add_node(network, access_node.node_id, freerange1, pool.pool_id)
-network_used_ranges.add(freerange1)
+if not network_node_ids:
+    freerange1 = get_free_iprange(ip_range, network_used_ranges)
+    zos.network.add_node(network, access_node.node_id, freerange1, pool.pool_id)
+    network_used_ranges.add(freerange1)
 
-freerange2 = get_free_iprange(ip_range, network_used_ranges)
-wg_quick = zos.network.add_access(network, access_node.node_id, freerange2, ipv4=True)
-network_used_ranges.add(freerange2)
-print(wg_quick)
-with open("ubuntu_deploy.conf", "w") as f:
-    f.writelines(wg_quick)
+    freerange2 = get_free_iprange(ip_range, network_used_ranges)
+    wg_quick = zos.network.add_access(network, access_node.node_id, freerange2, ipv4=True)
+    network_used_ranges.add(freerange2)
+    print(wg_quick)
+    with open("ubuntu_deploy.conf", "w") as f:
+        f.writelines(wg_quick)
 
-for workload in network.network_resources:
-    wid = zos.workloads.deploy(workload)
-    if not wait_workload(wid):
-        raise Exception(f"Failed to deploy, Workload ID: {wid}")
+    for workload in network.network_resources:
+        wid = zos.workloads.deploy(workload)
+        if not wait_workload(wid):
+            raise Exception(f"Failed to deploy, Workload ID: {wid}")
+
+    j.core.executors.run_local("wg-quick up ./ubuntu_deploy.conf")
+
+else:
+    input("Please make sure you have access to the management network on this machine. Press enter to continue")
 
 # get free nodes for deploying containers
+
 nodes = []
-random.shuffle(pool_nodes)
-for _, node in enumerate(pool_nodes):
+deployment_nodes = [get_deploymnet_node(pool)]
+bad_nodes.append(deployment_nodes[0].node_id)
+deployment_nodes.append(get_deploymnet_node(pool))
+
+for _, node in enumerate(deployment_nodes):
     if node.node_id in network_node_ids:
         nodes.append({"id": node.node_id, "ip_range": node_ranges[node.node_id]})
         continue
@@ -159,8 +212,9 @@ for workload in node_workloads.values():
     if not wait_workload(wid):
         raise Exception(f"Failed to deploy network for ubuntu, Workload ID: {wid}")
 
-ip_container_1 = get_node_free_ip(nodes[0]["ip_range"])
-ip_container_2 = get_node_free_ip(nodes[1]["ip_range"])
+network = zos.network.load_network(network_name)
+
+ip_container_1 = get_free_ip(network, deployment_nodes[0].node_id)
 
 # Deploy container 1
 container = zos.container.create(
@@ -175,11 +229,16 @@ container = zos.container.create(
     public_ipv6=True,
 )
 result = zos.workloads.deploy(container)
-j.sals.reservation_chatflow.deployer.wait_workload(result)
+success = j.sals.reservation_chatflow.deployer.wait_workload(result)
 workload = zos.workloads.get(result)
+if not success:
+    raise j.exceptions.Runtime(
+        f"failed to deploy container workload {result} due to error: {workload.info.result.message}"
+    )
 print("First container deployed successfully")
 
 # Deploy container 2
+ip_container_2 = get_free_ip(network, deployment_nodes[1].node_id)
 container2 = zos.container.create(
     node_id=nodes[1]["id"],
     network_name=network_name,
@@ -192,18 +251,21 @@ container2 = zos.container.create(
     public_ipv6=True,
 )
 result = zos.workloads.deploy(container2)
-j.sals.reservation_chatflow.deployer.wait_workload(result)
+success = j.sals.reservation_chatflow.deployer.wait_workload(result)
 workload = zos.workloads.get(result)
+if not success:
+    raise j.exceptions.Runtime(
+        f"failed to deploy container workload {result} due to error: {workload.info.result.message}"
+    )
 print("Second container deployed successfully")
 
-_, config, _ = j.core.executors.run_local("sudo wg")
-if "wg_script" in config:
-    j.core.executors.run_local("wg-quick down /tmp/wg_script.conf")
 
-j.core.executors.run_local(f'echo "{wg_quick}" > /tmp/wg_script.conf')
-j.core.executors.run_local("wg-quick up /tmp/wg_script.conf")
+sleep(10)
+if j.clients.sshclient.find("ubuntu_script"):
+    j.clients.sshclient.delete("ubuntu_script")
 
-sleep(5)
+if j.clients.sshclient.find("ubuntu_script2"):
+    j.clients.sshclient.delete("ubuntu_script2")
 
 localclient = j.clients.sshclient.get("ubuntu_script")
 localclient.sshkey = "ubuntu_script"
