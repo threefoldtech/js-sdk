c347721f805b1580d625dc8710c2bf89b21894e2
Omar Elawady <omarelawady1998@gmail.com>
2020-11-05 13:31:46 +0200

    add hostname so gevent can resolve it

--- jumpscale/packages/tfgrid_solutions/scripts/threebot/entrypoint.sh
+++ jumpscale/packages/tfgrid_solutions/scripts/threebot/entrypoint.sh
@@ -3,6 +3,7 @@ SDK_PATH="/sandbox/code/github/threefoldtech/js-sdk"
 echo "[*] Enabling ssh access ..."
 ssh-keygen -A
 echo "127.0.0.1       localhost" > /etc/hosts
+echo "127.0.0.1       $(hostname)" >> /etc/hosts
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 chmod -R 500 /etc/ssh
 service ssh restart
