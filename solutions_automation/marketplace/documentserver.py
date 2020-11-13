4ee6d664886c9df185467f0aa704cd0d8cd0f33f
tobiaschielens <tobias.chielens@gmail.org>
2020-11-13 11:56:28 +0100

    fixed import in automation

--- solutions_automation/marketplace/documentserver.py
+++ solutions_automation/marketplace/documentserver.py
@@ -1,5 +1,5 @@
 from jumpscale.packages.marketplace.chats.documentserver import DocumentserverDeploy
-from utils.gedispatch import GedisChatBotPatch
+from solutions_automation.utils.gedispatch import GedisChatBotPatch
 
 
 class DocumentserverAutomated(GedisChatBotPatch, DocumentserverDeploy):
