e45a6dbb7f07cf84071028ee8e39cc5cc98e6d55
tobiaschielens <tobias.chielens@gmail.org>
2020-11-13 11:45:34 +0100

    Added filebrowser solution automation

--- solutions_automation/marketplace/filebrowser.py
+++ solutions_automation/marketplace/filebrowser.py
@@ -3,4 +3,6 @@ from solutions_automation.utils.gedispatch import GedisChatBotPatch
 
 
 class FilebrowserAutomated(GedisChatBotPatch, FilebrowserDeploy):
-    pass
+    DOCUMENTSERVER_URL = "Please add the url to a documentserver"
+
+    QS = {DOCUMENTSERVER_URL: "https://office.jimber.org"}
