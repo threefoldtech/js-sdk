de966bf4c4d910d59f39c37e1968794918e4eb98
samaradel <adels@greenitglobe.com>
2020-11-22 11:26:36 +0200

    VDC UI (#1712)

---
+++ jumpscale/packages/vdc/frontend/components/base/Sidebar.vue
@@ -0,0 +1,136 @@
+<template>
+  <v-card width="256">
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            <v-dialog v-model="dialog" persistent max-width="290">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on">
+                  <v-icon left color="primary">mdi-wallet</v-icon> {{ name }}
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="headline"> </v-card-title>
+                <v-card-text
+                  >Please send TFT tokens to this wallet to fund your vdc.
+                  <br />
+                  <br />
+                  <span class="mr-2">Address: {{ address }}</span>
+                  <v-tooltip top>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-icon
+                        left
+                        color="primary"
+                        v-bind="attrs"
+                        v-on="on"
+                        @click.stop.prevent="copy"
+                        >mdi-content-copy</v-icon
+                      ><input type="hidden" id="copy-text" :value="address" />
+                    </template>
+                    <span>Copy</span>
+                  </v-tooltip>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="red darken-1" text @click="dialog = false">
+                    Close
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item
+          link
+          v-for="(val, key, id) in vdc"
+          :key="id"
+          :class="{ active: activeIndex === id }"
+          @click="setActive(id)"
+        >
+          <v-list-item-content>
+            <v-list-item-title class="text-capitalize">
+              <router-link class="d-block" :to="`/workloads/${name}/${key}`">
+                {{ key }}</router-link
+              >
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+module.exports = {
+  name: "Sidebar",
+  props: ["name", "vdc"],
+  data() {
+    return {
+      activeIndex: undefined,
+      dialog: false,
+      address: "THEADDRESSS",
+    };
+  },
+  methods: {
+    setActive(index) {
+      this.activeIndex = index;
+    },
+    copy() {
+      let testingCodeToCopy = document.querySelector("#copy-text");
+      testingCodeToCopy.setAttribute("type", "text");
+      testingCodeToCopy.select();
+      let successful = document.execCommand("copy");
+      testingCodeToCopy.setAttribute("type", "hidden");
+      window.getSelection().removeAllRanges();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-list-item__title a {
+  text-decoration: none;
+  font-size: 1rem;
+  padding: 5px 10px;
+}
+
+.active {
+  background-color: #1b4f72;
+}
+
+.active .v-list-item__title a {
+  color: #fff;
+}
+
+.v-list--nav .v-list-item {
+  border-radius: 0;
+}
+
+.v-list--nav {
+  padding: 0;
+}
+
+.v-btn__content {
+  text-transform: initial;
+}
+
+.theme--light.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+  background-color: transparent;
+}
+
+.v-btn--contained {
+  box-shadow: none;
+}
+.v-btn:not(.v-btn--text):not(.v-btn--outlined):hover:before {
+  opacity: 0;
+}
+:focus {
+  outline: transparent;
+}
+</style>
--- jumpscale/packages/vdc/frontend/components/solutions/Workloads.vue
+++ jumpscale/packages/vdc/frontend/components/solutions/Workloads.vue
@@ -6,14 +6,14 @@
       :loading="loading"
     >
       <template #actions>
-        <v-btn color="primary" text @click.stop="openChatflow('threebot')">
-          <v-icon left>mdi-plus</v-icon>New
+        <v-btn color="primary" text>
+          <v-icon left>mdi-plus</v-icon>Add a new VDC
         </v-btn>
       </template>
       <div class="mt-5">
         <template>
           <deployer-data-table
-            :deployed="deployed3Bots"
+            :deployed="deployedvdcs"
             :headers="headers3Bots"
             :loading="loading"
           >
@@ -34,11 +34,104 @@ module.exports = {
       threebot_data: APPS["threebot"],
       loading: true,
       headers3Bots: [
+        { text: "ID", value: "id" },
         { text: "Name", value: "name" },
-        { text: "State", value: "state"},
+        { text: "Package", value: "package" },
+        { text: "Expiration", value: "expiration" },
         { text: "Actions", value: "actions", sortable: false },
       ],
-      deployed3Bots: [],
+      deployedvdcs: [
+        {
+          vdc_name: "testvdc9",
+          owner_tname: "testoooo09",
+          solution_uuid: "0b5754eb1e324663971746fd2e78511a",
+          identity_tid: 2728,
+          flavor: "silver",
+          s3: {
+            subdomain: "s3-testvdc9-testoooo09.tfgw-testnet-01.gateway.tf",
+            healer_subdomain:
+              "s3-testvdc9-testoooo09-healing.tfgw-testnet-01.gateway.tf",
+            minio: {
+              ip_address: "10.200.2.3",
+              wid: 506628,
+              node_id: "DNav25NSCpx8NHwX11CUoUMsr2VW42sGoFCx6dtxyMV9",
+              pool_id: 5695,
+            },
+            zdbs: [
+              {
+                wid: 506612,
+                node_id: "8BFWcddW79cawvjnuUMw8M5s9FqS3pHhMHvzn3pgCUiB",
+                pool_id: 5695,
+              },
+              {
+                wid: 506613,
+                node_id: "6m9gFPTYLRiCH7FD8yU5VWwXZdfegKUvXZgrJoS1EJfQ",
+                pool_id: 5695,
+              },
+              {
+                wid: 506614,
+                node_id: "3dAnxcykEDgKVQdTRKmktggL2MZbm3CPSdS9Tdoy4HAF",
+                pool_id: 5695,
+              },
+            ],
+            api_proxy: {
+              reverse_proxy: {
+                wid: 506631,
+                node_id: "6RZfnjuXVLFdtZh218hn4BLzsoKkTVpweqWECk5YUKud",
+                pool_id: 5568,
+              },
+              subdomain: {
+                wid: 506630,
+                node_id: "6RZfnjuXVLFdtZh218hn4BLzsoKkTVpweqWECk5YUKud",
+                pool_id: 5568,
+              },
+              nginx: {
+                wid: 506640,
+                node_id: "2tzdPqM5xrFHkn264GHyUzXQda9UxFGcWMR9BeN54FTi",
+                pool_id: 5695,
+              },
+            },
+            healer_proxy: {
+              reverse_proxy: {
+                wid: 506643,
+                node_id: "6RZfnjuXVLFdtZh218hn4BLzsoKkTVpweqWECk5YUKud",
+                pool_id: 5568,
+              },
+              subdomain: {
+                wid: 506642,
+                node_id: "6RZfnjuXVLFdtZh218hn4BLzsoKkTVpweqWECk5YUKud",
+                pool_id: 5568,
+              },
+              nginx: {
+                wid: 506644,
+                node_id: "DNav25NSCpx8NHwX11CUoUMsr2VW42sGoFCx6dtxyMV9",
+                pool_id: 5695,
+              },
+            },
+          },
+          kubernetes: [
+            {
+              role: "master",
+              ip_address: "10.200.2.2",
+              size: 1,
+              wid: 506619,
+              node_id: "DNav25NSCpx8NHwX11CUoUMsr2VW42sGoFCx6dtxyMV9",
+              pool_id: 5695,
+            },
+            {
+              role: "worker",
+              ip_address: "10.200.3.2",
+              size: 1,
+              wid: 506626,
+              node_id: "8BFWcddW79cawvjnuUMw8M5s9FqS3pHhMHvzn3pgCUiB",
+              pool_id: 5695,
+            },
+          ],
+          created: 1605698806,
+          updated: 1605698806,
+          expiration: 1608290806,
+        },
+      ],
     };
   },
   methods: {
@@ -69,9 +162,9 @@ module.exports = {
       this.$api.solutions
         .getAllThreebots()
         .then((response) => {
-          this.deployed3Bots = [...response.data.data];
-          for (let i = 0; i < this.deployed3Bots.length; i++) {
-            deployed3Bot = this.deployed3Bots[i];
+          this.deployedvdcs = [...response.data.data];
+          for (let i = 0; i < this.deployedvdcs.length; i++) {
+            deployed3Bot = this.deployedvdcs[i];
             deployed3Bot.alert = false;
             if (deployed3Bot.expiration < DURATION_MAX) {
               let expiration = new Date(deployed3Bot.expiration * 1000);
