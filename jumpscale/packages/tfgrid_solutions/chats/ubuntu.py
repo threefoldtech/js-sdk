481c543b61d2f6084d9e6958830aedf48e9b5afc
m-motawea <magedmotawea@gmail.com>
2020-11-22 14:03:38 +0200

    fix ssh key cleaning

--- jumpscale/packages/tfgrid_solutions/chats/ubuntu.py
+++ jumpscale/packages/tfgrid_solutions/chats/ubuntu.py
@@ -91,7 +91,7 @@ class UbuntuDeploy(GedisChatBot):
     def public_key_get(self):
         self.public_key = self.upload_file(
             """Please upload your public SSH key to be able to access the depolyed container via ssh""", required=True,
-        ).split("\n")[0]
+        ).strip()
 
     @chatflow_step(title="Global IPv6 Address")
     def ipv6_config(self):
