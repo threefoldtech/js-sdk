417c11bb2ac54f2be145e19a1d5226809f4921c4
ranatrk <rana.t.hassan@gmail.com>
2020-11-23 18:32:02 +0200

    Update solution listing in marketplacevdc actors kubectl usage to include diff kubeconfig location

--- jumpscale/packages/marketplacevdc/actors/solutions.py
+++ jumpscale/packages/marketplacevdc/actors/solutions.py
@@ -15,10 +15,13 @@ class Solutions(BaseActor):
         # TODO loop over all vdc instances and get config path
         vdc_info = {}  # TODO to be removed
         vdc_info["kube_config_path"] = f"{j.core.dirs.HOMEDIR}/.kube/config"  # TODO to be removed
+        config_path = vdc_info["kube_config_path"]
         k8s_client = j.sals.kubernetes.Manager(config_path=vdc_info["kube_config_path"])
 
         # Get all deployments
-        kubectl_deployment_info = k8s_client.execute_native_cmd(cmd=f"kubectl get deployments -o json")
+        kubectl_deployment_info = k8s_client.execute_native_cmd(
+            cmd=f"kubectl --kubeconfig {config_path} get deployments -o json"
+        )
         deployments = j.data.serializers.json.loads(kubectl_deployment_info)["items"]
 
         for deployment_info in deployments:
@@ -40,11 +43,12 @@ class Solutions(BaseActor):
         # TODO loop over all vdc instances and get config path
         vdc_info = {}  # TODO to be removed
         vdc_info["kube_config_path"] = f"{j.core.dirs.HOMEDIR}/.kube/config"  # TODO to be removed
+        config_path = vdc_info["kube_config_path"]
         k8s_client = j.sals.kubernetes.Manager(config_path=vdc_info["kube_config_path"])
         # Get deployments for specific solution type in k8s cluster
         # returns a json string
         kubectl_deployment_info = k8s_client.execute_native_cmd(
-            cmd=f'kubectl get deployments -o=jsonpath=\'{{range .items[?(@.metadata.labels.app\.kubernetes\.io/name=="{solution_type}")]}}{{@}}{{"\\n"}}{{end}}\''
+            cmd=f'kubectl --kubeconfig {config_path} get deployments -o=jsonpath=\'{{range .items[?(@.metadata.labels.app\.kubernetes\.io/name=="{solution_type}")]}}{{@}}{{"\\n"}}{{end}}\''
         )
         # TODO Improve splitting
         deployments = kubectl_deployment_info.split("\n")[:-1]
