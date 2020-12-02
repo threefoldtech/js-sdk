9c7cabb05b7fa90a158d258eafd96d8d5a466d8b
Omar Elawady <omarelawady1998@gmail.com>
2020-12-02 12:08:17 +0200

    move init logic out of the entrypoint

--- jumpscale/packages/tfgrid_solutions/scripts/threebot/entrypoint.sh
+++ jumpscale/packages/tfgrid_solutions/scripts/threebot/entrypoint.sh
@@ -27,16 +27,26 @@ if $poetry_install; then
   poetry install
 fi
 
-echo "INSTANCE_NAME=${INSTANCE_NAME}" >> ~/.bashrc
-echo "THREEBOT_NAME=${THREEBOT_NAME}" >> ~/.bashrc
-echo "BACKUP_PASSWORD=${BACKUP_PASSWORD}" >> ~/.bashrc
-echo "BACKUP_TOKEN=${BACKUP_TOKEN}" >> ~/.bashrc
-echo "DOMAIN=${DOMAIN}" >> ~/.bashrc
+# Execute the initialization script from the newly fetched branch
+[ -f ./initialize.sh ] && bash ./initialize.sh
 
-echo "EMAIL_HOST=${EMAIL_HOST}" >> ~/.bashrc
-echo "EMAIL_HOST_USER=${EMAIL_HOST_USER}" >> ~/.bashrc
-echo "EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}" >> ~/.bashrc
-echo "ESCALATION_MAIL=${ESCALATION_MAIL}" >> ~/.bashrc
 
-echo "[*] Starting threebot in background ..."
-python3 jumpscale/packages/tfgrid_solutions/scripts/threebot/entrypoint.py
+# If the initialization script doesn't exist,
+# this branch is not updated with this script.
+# So we'll execute the original logic of the development
+# branch to keep them working without any changes
+if ! [ -f ./initialize.sh ]; then
+  echo "INSTANCE_NAME=${INSTANCE_NAME}" >> ~/.bashrc
+  echo "THREEBOT_NAME=${THREEBOT_NAME}" >> ~/.bashrc
+  echo "BACKUP_PASSWORD=${BACKUP_PASSWORD}" >> ~/.bashrc
+  echo "BACKUP_TOKEN=${BACKUP_TOKEN}" >> ~/.bashrc
+  echo "DOMAIN=${DOMAIN}" >> ~/.bashrc
+
+  echo "EMAIL_HOST=${EMAIL_HOST}" >> ~/.bashrc
+  echo "EMAIL_HOST_USER=${EMAIL_HOST_USER}" >> ~/.bashrc
+  echo "EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}" >> ~/.bashrc
+  echo "ESCALATION_MAIL=${ESCALATION_MAIL}" >> ~/.bashrc
+
+  echo "[*] Starting threebot in background ..."
+  python3 jumpscale/packages/tfgrid_solutions/scripts/threebot/entrypoint.py
+fi
