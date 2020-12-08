6c863b67d65f39c28cacfe539653f6b782e8deb9
m-motawea <magedmotawea@gmail.com>
2020-12-08 15:29:44 +0200

    load from github

--- jumpscale/sals/vdc/size.py
+++ jumpscale/sals/vdc/size.py
@@ -19,17 +19,26 @@ PREFERED_FARM = "lochristi_dev_lab"
 NETWORK_FARM = "lochristi_dev_lab"
 
 
+# TODO: use the correct url
+WORKLOAD_SIZES_URL = (
+    "https://raw.githubusercontent.com/threefoldfoundation/vdc_pricing/development_dynamic_size/workload_sizes.json"
+)
+PLANS_URL = "https://raw.githubusercontent.com/threefoldfoundation/vdc_pricing/development_dynamic_size/plans.json"
+
+
 class VDCSize:
     _LAST_LOADED = None
 
     def __init__(self):
         self.K8SNodeFlavor = None
-        self.K8S_PRICES = None
+        self.K8S_PRICES = None  # TODO: remove
         self.K8S_SIZES = None
         self.S3ZDBSize = None
         self.S3_ZDB_SIZES = None
         self.VDCFlavor = None
         self.VDC_FLAVORS = None
+        self._workload_sizes = None
+        self._plans_data = None
 
     @staticmethod
     def _convert_eur_to_tft(amount):
@@ -58,83 +67,74 @@ class VDCSize:
         elif self._LAST_LOADED < j.data.time.now() + (24 * 60 * 60):
             update_flag = True
         if update_flag:
+            self.fetch_upstream_info()
             self.load_k8s_flavor()
-            self.load_k8s_prices()
             self.load_k8s_sizes()
             self.load_s3_zdb_size()
             self.load_s3_zdb_size_details()
             self.load_vdc_flavors()
             self.load_vdc_plans()
 
+    def fetch_upstream_info(self):
+        workloads_res = j.tools.http.get(WORKLOAD_SIZES_URL)
+        workloads_res.raise_for_status()
+        self._workload_sizes = workloads_res.json()
+
+        plans_res = j.tools.http.get(PLANS_URL)
+        plans_res.raise_for_status()
+        self._plans_data = plans_res.json()
+
     def load_k8s_flavor(self):
         # fills K8SNodeFlavor
-        self.K8SNodeFlavor = Enum("K8SNodeFlavor", {"SMALL": 15, "MEDIUM": 16, "BIG": 17,})
-
-    def load_k8s_prices(self):
-        # fills K8S_PRICES
-        self.K8S_PRICES = {self.K8SNodeFlavor.SMALL: 5, self.K8SNodeFlavor.MEDIUM: 10, self.K8SNodeFlavor.BIG: 20}
+        values = dict()
+        for key, val in self._workload_sizes["kubernetes"].items():
+            values[key.upper()] = val["value"]
+        self.K8SNodeFlavor = Enum("K8SNodeFlavor", values)
 
     def load_k8s_sizes(self):
         # fills K8S_SIZES
-        self.K8S_SIZES = {
-            self.K8SNodeFlavor.SMALL: {"cru": 1, "mru": 2, "sru": 25},
-            self.K8SNodeFlavor.MEDIUM: {"cru": 2, "mru": 4, "sru": 50},
-            self.K8SNodeFlavor.BIG: {"cru": 4, "mru": 8, "sru": 50},
-        }
+        self.K8S_SIZES = dict()
+        for key, val in self._workload_sizes["kubernetes"].items():
+            self.K8S_SIZES[self.K8SNodeFlavor[key.upper()]] = {
+                "cru": val["cru"],
+                "mru": val["mru"],
+                "sru": val["sru"],
+            }
 
     def load_s3_zdb_size(self):
         # fills S3ZDBSize
-        self.S3ZDBSize = Enum("S3ZDBSize", {"SMALL": 1, "MEDIUM": 2, "BIG": 3,})
+        values = dict()
+        for key, val in self._workload_sizes["zdb"].items():
+            values[key.upper()] = val["value"]
+        self.S3ZDBSize = Enum("S3ZDBSize", values)
 
     def load_s3_zdb_size_details(self):
         # fills S3_ZDB_SIZES
-        self.S3_ZDB_SIZES = {
-            self.S3ZDBSize.SMALL: {"sru": 3 * 1024},
-            self.S3ZDBSize.MEDIUM: {"sru": 10 * 1024},
-            self.S3ZDBSize.BIG: {"sru": 20 * 1024},
-        }
+        self.S3_ZDB_SIZES = dict()
+        for key, val in self._workload_sizes["zdb"].items():
+            self.S3_ZDB_SIZES[self.S3ZDBSize[key.upper()]] = {"sru": val["sru"]}
 
     def load_vdc_flavors(self):
         # fills VDCFlavor
-        self.VDCFlavor = Enum("VDCFlavor", {"SILVER": "silver", "GOLD": "gold", "PLATINUM": "platinum",})
+        values = dict()
+        for key in self._plans_data:
+            values[key.upper()] = key
+        self.VDCFlavor = Enum("VDCFlavor", values)
 
     def load_vdc_plans(self):
         # fills VDC_FLAVORS
-        self.VDC_FLAVORS = {
-            self.VDCFlavor.SILVER: {
+        self.VDC_FLAVORS = dict()
+        for key, val in self._plans_data.items():
+            self.VDC_FLAVORS[self.VDCFlavor[key.upper()]] = {
                 "k8s": {
-                    "no_nodes": 1,
-                    "size": self.K8SNodeFlavor.SMALL,
-                    "controller_size": self.K8SNodeFlavor.SMALL,
-                    "dedicated": False,
+                    "no_nodes": val["k8s"]["no_nodes"],
+                    "size": self.K8SNodeFlavor[val["k8s"]["size"].upper()],
+                    "controller_size": self.K8SNodeFlavor[val["k8s"]["controller_size"].upper()],
+                    "dedicated": val["k8s"]["dedicated"],
                 },
-                "s3": {"size": self.S3ZDBSize.SMALL},
-                "duration": 30,  # days
-                "price": 30,
-            },
-            self.VDCFlavor.GOLD: {
-                "k8s": {
-                    "no_nodes": 2,
-                    "size": self.K8SNodeFlavor.MEDIUM,
-                    "controller_size": self.K8SNodeFlavor.MEDIUM,
-                    "dedicated": False,
-                },
-                "s3": {"size": self.S3ZDBSize.MEDIUM},
-                "duration": 30,  # days
-                "price": 60,
-            },
-            self.VDCFlavor.PLATINUM: {
-                "k8s": {
-                    "no_nodes": 2,
-                    "size": self.K8SNodeFlavor.BIG,
-                    "controller_size": self.K8SNodeFlavor.BIG,
-                    "dedicated": True,
-                },
-                "s3": {"size": self.S3ZDBSize.BIG},
-                "duration": 30,  # days
-                "price": 250,
-            },
-        }
+                "s3": {"size": self.S3ZDBSize[val["s3"]["size"].upper()]},
+                "duration": val["duration"],
+            }
 
 
 VDC_SIZE = VDCSize()
