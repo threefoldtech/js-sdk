a5fcc2d97883eefef2aa303dccba050b28becacb
m-motawea <magedmotawea@gmail.com>
2020-12-17 12:36:41 +0200

    fix external node payment

--- jumpscale/sals/vdc/vdc.py
+++ jumpscale/sals/vdc/vdc.py
@@ -275,6 +275,8 @@ class UserVDC(Base):
         return j.sals.billing.wait_payment(payment_id, bot=bot), amount, payment_id
 
     def show_external_node_payment(self, bot, size, no_nodes=1, expiry=5, wallet_name=None, public_ip=False):
+        if isinstance(size, str):
+            size = VDC_SIZE.K8SNodeFlavor[size.upper()]
         amount = VDC_SIZE.PRICES["nodes"][size] * no_nodes
         if public_ip:
             amount += VDC_SIZE.PRICES["services"][VDC_SIZE.Services.IP]
