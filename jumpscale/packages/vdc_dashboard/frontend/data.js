77a40661c1a11fb6d670f8ef450e999157abffbd
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-12-27 16:42:01 +0200

    fixing typo in frontend

--- jumpscale/packages/vdc_dashboard/frontend/data.js
+++ jumpscale/packages/vdc_dashboard/frontend/data.js
@@ -90,7 +90,6 @@ const DECENTRALIZED_WE = {
             helpLink: "https://now.threefold.io/now/docs/discourse/",
             description: "Discourse is an open source Internet forum and mailing list management software application built to educate members about civil community engagement."
         },
-        },
         kubeapps: {
             name: "Kubeapps",
             type: "kubeapps",
