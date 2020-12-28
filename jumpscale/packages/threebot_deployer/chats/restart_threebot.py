ac1d54f6fbaf6feb2e10386afe7502092a0e1981
Waleed <waleed.hammam@gmail.com>
2020-12-28 11:25:40 +0200

    fix subdomain deletion waiting (#2013)
    fix 3bot extension and restart

--- jumpscale/packages/threebot_deployer/chats/restart_threebot.py
+++ jumpscale/packages/threebot_deployer/chats/restart_threebot.py
@@ -13,7 +13,7 @@ from jumpscale.loader import j
 
 
 class ThreebotRedeploy(MarketPlaceAppsChatflow):
-    FLIST_URL = "https://hub.grid.tf/ahmed_hanafy_1/ahmedhanafy725-js-sdk-latest.flist"
+    FLIST_URL = "https://hub.grid.tf/ahmed_hanafy_1/ahmedhanafy725-js-sdk-latest_trc.flist"
     SOLUTION_TYPE = "threebot"  # chatflow used to deploy the solution
     title = "3Bot"
     steps = [
@@ -39,9 +39,9 @@ class ThreebotRedeploy(MarketPlaceAppsChatflow):
         self.threebot_info = self.non_running_names[self.name]
         self.pool_id = self.threebot_info["compute_pool"]
         self.query = {
-            "cru": self.threebot_info["cpu"] + 1,
-            "mru": self.threebot_info["memory"] / 1024 + 1,
-            "sru": self.threebot_info["disk_size"] / 1024 + 0.25,
+            "cru": self.threebot_info["cpu"],
+            "mru": self.threebot_info["memory"] / 1024,
+            "sru": self.threebot_info["disk_size"] / 1024,
         }
 
     @chatflow_step(title="New Expiration")
