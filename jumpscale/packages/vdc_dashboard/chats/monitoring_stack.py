00ff4f1d431db5bd01fba6e5c9538c6c9215291a
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-12-29 14:20:49 +0200

    update monitor stack chat flow

--- jumpscale/packages/vdc_dashboard/chats/monitoring_stack.py
+++ jumpscale/packages/vdc_dashboard/chats/monitoring_stack.py
@@ -9,29 +9,33 @@ class InstallMonitoringStack(SolutionsChatflowDeploy):
     steps = ["confirm", "success"]
     SOLUTION_TYPE = "monitoring_stack"
     HELM_REPO_NAME = "marketplace"
+    steps = [
+        "get_release_name",
+        "create_prometheus_subdomain",
+        "create_grafane_subdomain",
+        "set_config",
+        "install_chart",
+        "initializing",
+        "success",
+    ]
 
-    @chatflow_step(title="Confirmation")
-    def confirm(self):
-        vdc_full_name = list(j.sals.vdc.list_all())[0]
-        self.vdc = VDCFACTORY.find(vdc_full_name, load_info=True)
-        vdc_deployer = self.vdc.get_deployer()
-        self.public_ip = self.single_choice(
-            "Do you want to Deploy the monitoring stack which contains (Grafana, Prometheus, Redis) on your VDC?",
-            options=["Yes", "No"],
-            default="Yes",
-            required=True,
-        )
-        if self.public_ip == "No":
-            self.stop("The selected option was No, Please go back to the marketplace to install other solutions.")
-        else:
-            try:
-                vdc_deployer.monitoring.deploy_stack()
-            except j.exceptions.Runtime as e:
-                self.stop(message=f"failed to deploy monitoring stack due to error {str(e)}")
+    @chatflow_step(title="Create Prometheus subdomain")
+    def create_prometheus_subdomain(self):
+        {super().create_subdomain()}
 
-    @chatflow_step(title="Success", disable_previous=True, final_step=True)
-    def success(self):
-        self.md_show(f"""You have deployed the monitoring stack on VDC: {self.vdc.vdc_name} successfuly""")
+    @chatflow_step(title="Create Grafana subdomain")
+    def create_grafane_subdomain(self):
+        {super().create_subdomain()}
+
+    @chatflow_step(title="Configurations")
+    def set_config(self):
+        self._choose_flavor()
+        prometheus_domain = self.domain
+        grafana_domain = self.domain
+        self.chart_config = {
+            "prometheus.ingress.hosts[0]": prometheus_domain,
+            "grafana.ingress.hosts[0]": grafana_domain,
+        }
 
 
 chat = InstallMonitoringStack
