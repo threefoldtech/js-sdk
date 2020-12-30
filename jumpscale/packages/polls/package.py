0680299f7608d48eb0a06c8e41ea66adf5a0d917
Waleed <waleed.hammam@gmail.com>
2020-12-30 13:42:21 +0200

    migrate polls package to use STD wallet

--- jumpscale/packages/polls/package.py
+++ jumpscale/packages/polls/package.py
@@ -1,13 +1,11 @@
 from jumpscale.loader import j
+
+
 class polls:
     def install(self, **kwargs):
         if "polls_receive" not in j.clients.stellar.list_all():
             secret = kwargs.get("secret", None)
-            network = kwargs.get("network", "TEST")
-            wallet = j.clients.stellar.new("polls_receive", secret=secret, network=network)
+            wallet = j.clients.stellar.new("polls_receive", secret=secret)
             if not secret:
-                if network == "TEST":
-                    wallet.activate_through_friendbot()
-                else:
-                    wallet.activate_through_threefold_service()
+                wallet.activate_through_threefold_service()
             wallet.save()
