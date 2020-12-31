c5d9d3cec846bd4e68d21b0435c60f0402e67ac0
Waleed <waleed.hammam@gmail.com>
2021-01-01 00:22:16 +0200

    fix wallet creation in vdcs

--- jumpscale/sals/vdc/wallet.py
+++ jumpscale/sals/vdc/wallet.py
@@ -2,8 +2,6 @@ from jumpscale.clients.stellar.stellar import Network as StellarNetwork
 from jumpscale.core.base import Base, StoredFactory, fields
 from jumpscale.loader import j
 
-INIT_WALLET_NAME = j.core.config.get("VDC_INIT", "vdc_init")
-
 
 class VDCWallet(Base):
     vdc_uuid = fields.String()
@@ -11,23 +9,18 @@ class VDCWallet(Base):
     wallet_network = fields.Enum(StellarNetwork)
 
     def _init_wallet(self, secret=None):
-        wallet = j.clients.stellar.find(INIT_WALLET_NAME)
-        if not wallet:
-            wallet = j.clients.stellar.new(self.instance_name, secret=secret)
-            if not secret:
-                wallet.activate_through_threefold_service()
-                wallet.add_known_trustline("TFT")
+        wallet = j.clients.stellar.new(self.instance_name, secret=secret)
+        if not secret:
+            wallet.activate_through_threefold_service()
+            wallet.add_known_trustline("TFT")
         wallet.save()
         self.wallet_secret = wallet.secret
 
     @property
     def stellar_wallet(self):
-        wallet = j.clients.stellar.find(INIT_WALLET_NAME)
-        if not wallet:
-            if not j.clients.stellar.find(self.instance_name) and self.wallet_secret:
-                self._init_wallet(self.wallet_secret)
-            return j.clients.stellar.get(self.instance_name)
-        return wallet
+        if not j.clients.stellar.find(self.instance_name) and self.wallet_secret:
+            self._init_wallet(self.wallet_secret)
+        return j.clients.stellar.get(self.instance_name)
 
 
 class VDCWalletStoredFactory(StoredFactory):
