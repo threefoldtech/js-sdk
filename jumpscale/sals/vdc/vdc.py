3fcbef774870b585a3d0fbffa3e20253068c597d
Waleed <waleed.hammam@gmail.com>
2020-12-31 15:43:01 +0200

    round price and remove trc container (#2090)

--- jumpscale/sals/vdc/vdc.py
+++ jumpscale/sals/vdc/vdc.py
@@ -305,7 +305,7 @@ class UserVDC(Base):
 
     def show_vdc_payment(self, bot, expiry=5, wallet_name=None):
         discount = FARM_DISCOUNT.get()
-        amount = VDC_SIZE.PRICES["plans"][self.flavor] * (1 - discount)
+        amount = round(VDC_SIZE.PRICES["plans"][self.flavor] * (1 - discount))
 
         payment_id, _ = j.sals.billing.submit_payment(
             amount=amount,
