f0808a818560f54e8058d5c4e40af7d947f4ab0f
Abdelrahman Ghanem <abom.jdev@gmail.com>
2021-01-05 16:16:03 +0200

    update publishingtools chatflows to use charts (#2083)
    * update publishingtools chatflows to use charts
    
    * vdc: fix publisher chatflows
    
    * pass domain via ingress.host

--- jumpscale/packages/vdc_dashboard/chats/wiki.py
+++ jumpscale/packages/vdc_dashboard/chats/wiki.py
@@ -1,30 +1,35 @@
-from jumpscale.packages.marketplace.chats.publisher import Publisher
+from jumpscale.packages.vdc_dashboard.chats.publisher import Publisher
 from jumpscale.sals.chatflows.chatflows import chatflow_step
 
 
 class WikiDeploy(Publisher):
-
-    title = "Deploy a Wiki"
-    SOLUTION_TYPE = "wiki"  # chatflow used to deploy the solution
     EXAMPLE_URL = "https://github.com/threefoldfoundation/wiki_example"
 
-    @chatflow_step(title="Wiki Setup")
-    def configuration(self):
-        self.user_email = self.user_info()["email"]
+    title = "Deploy a Wiki"
+
+    @chatflow_step(title="Configurations")
+    def set_config(self):
+        self._choose_flavor()
+
         form = self.new_form()
         title = form.string_ask("Title", required=True)
         url = form.string_ask("Repository URL", required=True, is_git_url=True)
         branch = form.string_ask("Branch", required=True)
+        srcdir = form.string_ask("Source directory", required=False, default="")
         msg = self.get_mdconfig_msg()
         form.ask(msg, md=True)
-        self.envars = {
-            "TYPE": "wiki",
-            "NAME": "entrypoint",
-            "TITLE": title.value,
-            "URL": url.value,
-            "BRANCH": branch.value,
-            "EMAIL": self.user_email,
-        }
+        self.chart_config.update(
+            {
+                "env.type": "wiki",
+                "env.title": title.value,
+                "env.url": url.value,
+                "env.branch": branch.value,
+                "env.srcdir": srcdir.value,
+                "ingress.host": self.domain,
+                "resources.limits.cpu": self.resources_limits["cpu"],
+                "resources.limits.memory": self.resources_limits["memory"],
+            }
+        )
 
 
 chat = WikiDeploy
