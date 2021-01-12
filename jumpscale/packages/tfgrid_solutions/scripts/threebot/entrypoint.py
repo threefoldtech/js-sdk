f923b4b6cdbf20d874f61d717f36beb0ea65ee85
Waleed <waleed.hammam@gmail.com>
2021-01-12 20:28:11 +0200

    fix wallet in entrypoint if not created on the deployer

--- jumpscale/packages/tfgrid_solutions/scripts/threebot/entrypoint.py
+++ jumpscale/packages/tfgrid_solutions/scripts/threebot/entrypoint.py
@@ -93,12 +93,12 @@ def main():
 
     # get the main wallet
     j.logger.info("Initalizing main wallet ...")
-    if WALLET_SECRET:
+    try:
         wallet = j.clients.stellar.get("main")
         wallet.secret = WALLET_SECRET
         wallet.save()
-    else:
-        j.logger.error(f"Failed to create wallet, secret wasn't passed correctly ...")
+    except Exception as e:
+        j.logger.error(f"Failed to create wallet, secret wasn't passed correctly: {str(e)}...")
 
     j.logger.info("Starting threebot ...")
 
