ba7c8b3c9648753bfdeb5ffd502b0b8cf82c5cc8
abdulgig <a.rehmans@gmail.com>
2021-01-14 09:18:48 +0000

    add digibyte chatflow

---
+++ jumpscale/packages/vdc_dashboard/chats/digibyte.py
@@ -0,0 +1,24 @@
+from jumpscale.sals.chatflows.chatflows import chatflow_step
+from jumpscale.packages.vdc_dashboard.sals.solutions_chatflow import SolutionsChatflowDeploy
+
+
+class CryptpadDeploy(SolutionsChatflowDeploy):
+    SOLUTION_TYPE = "digibyte"
+    title = "Digibyte"
+    HELM_REPO_NAME = "marketplace"
+    steps = ["get_release_name", "set_config", "install_chart", "initializing", "success"]
+
+    @chatflow_step(title="Configurations")
+    def set_config(self):
+        self._choose_flavor()
+        self.chart_config.update(
+            {
+                "resources.limits.cpu": self.resources_limits["cpu"],
+                "resources.limits.memory": self.resources_limits["memory"],
+                self.vdc.kubernetes.add_traefik_entrypoint("rpc", "14022"),
+                self.vdc.kubernetes.add_traefik_entrypoint("p2p", "12024"),
+            }
+        )
+
+
+chat = DigibyteDeploy
