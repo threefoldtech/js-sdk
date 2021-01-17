3578d5ba188c25440b8dfb607c90d5927f7531ef
MElborolossy <mohammedelborolossy@gmail.com>
2021-01-17 11:48:08 +0200

    Initial Commit

--- tests/sals/vdc/test_kubernetes.py
+++ tests/sals/vdc/test_kubernetes.py
@@ -5,7 +5,6 @@ from parameterized import parameterized_class
 from .vdc_base import VDCBase
 
 
-# @parameterized_class(("flavor"), [("silver",), ("gold",), ("platinum",), ("diamond",)])
 @parameterized_class(("flavor"), [("silver",)])
 @pytest.mark.integration
 class TestVDC(VDCBase):
@@ -38,25 +37,8 @@ class TestVDC(VDCBase):
 
     @classmethod
     def tearDownClass(cls):
-        j.sals.vdc.delete(cls.vdc.instance_name)
+        # j.sals.vdc.delete(cls.vdc.instance_name) Todo: Uncomment before merging with development_vdc
         super().tearDownClass()
 
-    def test_01_list_vdcs(self):
-        """Test case for listing deployed vdcs.
-
-        **Test Scenario**
-
-        - Deploy VDC.
-        - List deployed vdcs.
-        - Check that the vdc has been deployed is in the list.
-        """
-        self.info("List deployed vdcs.")
-        vdcs = j.sals.vdc.list(self.tname)
-
-        self.info("Check that the vdc has been deployed is in the list.")
-        found = False
-        for vdc in vdcs:
-            if vdc.vdc_name == self.vdc_name:
-                found = True
-                break
-        self.assertTrue(found)
+    def test_01_add_helm_hepo(self):
+        pass
