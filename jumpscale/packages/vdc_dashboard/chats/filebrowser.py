5a3eb5d380e8edea3c6429af85c4af1093c0651d
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-01-18 12:05:15 +0200

    rename some variables

--- jumpscale/packages/vdc_dashboard/chats/filebrowser.py
+++ jumpscale/packages/vdc_dashboard/chats/filebrowser.py
@@ -10,10 +10,10 @@ class FileBrowser(SolutionsChatflowDeploy):
     HELM_REPO_NAME = "marketplace"
     steps = ["get_release_name", "create_subdomain", "get_documentserver_value","set_config", "install_chart", "initializing","success"]
    
-    @chatflow_step(title="Document server")
+    @chatflow_step(title="Document server URL")
     def get_documentserver_value(self):
         message = "Please enter Document Server URL"
-        self.document_url =  self.string_ask(message, required=True, is_identifier=False, md=True)
+        self.document_server_url =  self.string_ask(message, required=True, md=True)
 
     @chatflow_step(title="Configurations")
     def set_config(self):
@@ -21,7 +21,7 @@ class FileBrowser(SolutionsChatflowDeploy):
 
         self.chart_config.update({
             "extraEnvVars[0].name": "DOCUMENTSERVER_URL",
-            "extraEnvVars[0].value": self.document_url,
+            "extraEnvVars[0].value": self.document_server_url,
             "ingress.hosts[0]": self.domain,
             "resources.limits.cpu": self.resources_limits["cpu"],
             "resources.limits.memory": self.resources_limits["memory"],
