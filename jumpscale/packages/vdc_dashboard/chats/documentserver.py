3c9914c3e6cdf1c1ccc1d9e25468c2ca247b2103
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-01-19 12:34:09 +0200

    run black

--- jumpscale/packages/vdc_dashboard/chats/documentserver.py
+++ jumpscale/packages/vdc_dashboard/chats/documentserver.py
@@ -1,6 +1,8 @@
 from textwrap import dedent
 from jumpscale.sals.chatflows.chatflows import chatflow_step
-from jumpscale.packages.vdc_dashboard.sals.solutions_chatflow import SolutionsChatflowDeploy
+from jumpscale.packages.vdc_dashboard.sals.solutions_chatflow import (
+    SolutionsChatflowDeploy,
+)
 import json
 
 
@@ -8,16 +10,26 @@ class DocumentServer(SolutionsChatflowDeploy):
     SOLUTION_TYPE = "documentserver"
     title = "Document Server"
     HELM_REPO_NAME = "marketplace"
-    steps = ["get_release_name", "create_subdomain", "set_config", "install_chart", "initializing","success"]
+    steps = [
+        "get_release_name",
+        "create_subdomain",
+        "set_config",
+        "install_chart",
+        "initializing",
+        "success",
+    ]
+
     @chatflow_step(title="Configurations")
     def set_config(self):
         self._choose_flavor()
 
-        self.chart_config.update({
-            "ingress.hosts[0]": self.domain,
-            "resources.limits.cpu": self.resources_limits["cpu"],
-            "resources.limits.memory": self.resources_limits["memory"],
-         })
+        self.chart_config.update(
+            {
+                "ingress.hosts[0]": self.domain,
+                "resources.limits.cpu": self.resources_limits["cpu"],
+                "resources.limits.memory": self.resources_limits["memory"],
+            }
+        )
 
 
 chat = DocumentServer
--- jumpscale/packages/vdc_dashboard/chats/filebrowser.py
+++ jumpscale/packages/vdc_dashboard/chats/filebrowser.py
@@ -1,6 +1,8 @@
 from textwrap import dedent
 from jumpscale.sals.chatflows.chatflows import chatflow_step
-from jumpscale.packages.vdc_dashboard.sals.solutions_chatflow import SolutionsChatflowDeploy
+from jumpscale.packages.vdc_dashboard.sals.solutions_chatflow import (
+    SolutionsChatflowDeploy,
+)
 import json
 
 
@@ -8,24 +10,34 @@ class FileBrowser(SolutionsChatflowDeploy):
     SOLUTION_TYPE = "filebrowser"
     title = "File Browser"
     HELM_REPO_NAME = "marketplace"
-    steps = ["get_release_name", "create_subdomain", "get_documentserver_value","set_config", "install_chart", "initializing","success"]
-   
+    steps = [
+        "get_release_name",
+        "create_subdomain",
+        "get_documentserver_value",
+        "set_config",
+        "install_chart",
+        "initializing",
+        "success",
+    ]
+
     @chatflow_step(title="Document server URL")
     def get_documentserver_value(self):
         message = "Please enter Document Server URL"
-        self.document_server_url =  self.string_ask(message, required=True, md=True)
+        self.document_server_url = self.string_ask(message, required=True, md=True)
 
     @chatflow_step(title="Configurations")
     def set_config(self):
         self._choose_flavor()
 
-        self.chart_config.update({
-            "extraEnvVars[0].name": "DOCUMENTSERVER_URL",
-            "extraEnvVars[0].value": self.document_server_url,
-            "ingress.hosts[0]": self.domain,
-            "resources.limits.cpu": self.resources_limits["cpu"],
-            "resources.limits.memory": self.resources_limits["memory"],
-         })
+        self.chart_config.update(
+            {
+                "extraEnvVars[0].name": "DOCUMENTSERVER_URL",
+                "extraEnvVars[0].value": self.document_server_url,
+                "ingress.hosts[0]": self.domain,
+                "resources.limits.cpu": self.resources_limits["cpu"],
+                "resources.limits.memory": self.resources_limits["memory"],
+            }
+        )
 
 
 chat = FileBrowser
