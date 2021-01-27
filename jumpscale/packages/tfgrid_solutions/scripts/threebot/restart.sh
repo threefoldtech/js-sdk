98f90d348076d412d079eff6a90b78f3f09f892c
Omar Elawady <omarelawady1998@gmail.com>
2021-01-27 11:57:10 +0200

    Send SIGKILL after 2 minutes if the server didn't stop with SIGTERM

--- jumpscale/packages/tfgrid_solutions/scripts/threebot/restart.sh
+++ jumpscale/packages/tfgrid_solutions/scripts/threebot/restart.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
 after=$1
 
 sleep $after
 zinit stop start
 # wait until dead
 zinit status start | grep Down
+
+start_time=`date +%s`
 while [[ $? == 1 ]]; do
     echo "waiting for server to stop"
+    current_time=`date +%s`
+    if [[ $current_time > $(($start_time + 120)) ]]; then
+      zinit kill start SIGKILL
+      current_time=$start_time # to not call sigkill twice
+    fi
     sleep 1
     zinit status start | grep Down
 done
