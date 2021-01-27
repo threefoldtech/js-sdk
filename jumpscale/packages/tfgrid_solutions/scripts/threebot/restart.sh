fcdc52ec38489564c17bcf24a5cf75bc760ed158
Omar Elawady <omarelawady1998@gmail.com>
2021-01-27 12:27:45 +0200

    wait on pid: 0 as the condition for stopping

--- jumpscale/packages/tfgrid_solutions/scripts/threebot/restart.sh
+++ jumpscale/packages/tfgrid_solutions/scripts/threebot/restart.sh
@@ -5,10 +5,10 @@ after=$1
 sleep $after
 zinit stop start
 # wait until dead
-zinit status start | grep Down
 
 start_time=`date +%s`
-while [[ $? == 1 ]]; do
+
+while ! zinit status start | grep -q 'pid: 0' ; do
     echo "waiting for server to stop"
     current_time=`date +%s`
     if [[ $current_time > $(($start_time + 120)) ]]; then
@@ -16,6 +16,5 @@ while [[ $? == 1 ]]; do
       current_time=$start_time # to not call sigkill twice
     fi
     sleep 1
-    zinit status start | grep Down
 done
 zinit start start
