90961c67e08eadc7d404028b27f95a82ffe1b43d
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-01-27 14:53:55 +0200

    Code refactoring as there was depublication

--- jumpscale/packages/tfgrid_solutions/actors/solutions.py
+++ jumpscale/packages/tfgrid_solutions/actors/solutions.py
@@ -60,15 +60,6 @@ class Solutions(BaseActor):
         return j.data.serializers.json.dumps({"result": True})
 
     @actor_method
-    def delete_node(self, wid) -> str:
-        workload = j.sals.zos.get().workloads.get(wid)
-        # Delete public ip
-        if workload.public_ip:
-            j.sals.zos.get().workloads.decomission(workload.public_ip)
-        j.sals.zos.get().workloads.decomission(wid)
-        return j.data.serializers.json.dumps({"result": wid})
-
-    @actor_method
     def list_pools(self, include_hidden) -> str:
         def pool_farm_from_cache(cache_dict, pool):
             for node_id in pool.node_ids:
@@ -124,6 +115,10 @@ class Solutions(BaseActor):
 
     @actor_method
     def cancel_workload(self, wid) -> bool:
+        workload = j.sals.zos.get().workloads.get(wid)
+        # Delete public ip
+        if workload.public_ip:
+            j.sals.zos.get().workloads.decomission(workload.public_ip)
         j.sals.zos.get().workloads.decomission(wid)
         return True
 
