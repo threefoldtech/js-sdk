18d0ea032c6d1a1664343660f9df6f0f23198fd1
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-01-27 14:17:23 +0200

    Handle listing nodes actions including info and delete acions

---
+++ jumpscale/packages/admin/frontend/components/solutions/NodeInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-dialog title="Application details" v-model="dialog" :loading="loading">
+    <template #default>
+      <json-renderer
+        title="App details"
+        :jsonobj="data"
+        :ignored="KeysIgnored"
+      ></json-renderer>
+    </template>
+    <template #actions>
+      <v-btn text @click="close">Close</v-btn>
+    </template>
+  </base-dialog>
+</template>
+
+<script>
+module.exports = {
+  data() {
+    return {
+      dialogs: {
+        cancelSolution: false,
+      },
+      tab: 0,
+      KeysIgnored: ["Status Details", "User Supplied Values"],
+    };
+  },
+  props: { data: Object },
+  mixins: [dialog],
+};
+</script>
