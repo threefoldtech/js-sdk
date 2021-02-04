49d19a52cc10578120b59a6d415802eaad6bc5b2
Ahmed Samir <asamir97@hotmail.com>
2021-02-04 12:05:36 +0200

    Supporting extra storage for cryptpad chart through k8s volumes (#2454)
    * Support volume for cryptpad chart
    
    * Change volume size value name
    
    * make volume size numerical
    
    * Add volume hostpath as chart config

--- jumpscale/packages/vdc_dashboard/chats/cryptpad.py
+++ jumpscale/packages/vdc_dashboard/chats/cryptpad.py
@@ -12,9 +12,15 @@ class CryptpadDeploy(SolutionsChatflowDeploy):
     def set_config(self):
         # TODO: get config from user
         self._choose_flavor()
+        choices = ["10", "15", "20"]
+        self.volume_size = self.single_choice(
+            "Please select your storage size (in GBs)", choices, required=True, default="10",
+        )
         self.chart_config.update(
             {
                 "ingress.host": self.domain,
+                "volume.size": self.volume_size,
+                "volume.hostPath": f"/persistent-data/{self.release_name}",
                 "resources.limits.cpu": self.resources_limits["cpu"],
                 "resources.limits.memory": self.resources_limits["memory"],
             }
