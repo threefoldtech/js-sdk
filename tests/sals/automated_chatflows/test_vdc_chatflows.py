33903ad30d4e7c8aecd9ddc398ac60a6488b77b8
Mostafa Hassan <mhost39@gmail.com>
2021-02-09 13:33:34 +0200

    add vdc test

--- tests/sals/automated_chatflows/test_vdc_chatflows.py
+++ tests/sals/automated_chatflows/test_vdc_chatflows.py
@@ -12,17 +12,6 @@ from parameterized import parameterized_class
 class VDCChatflows(VDCBase):
     flavor = "silver"
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        wallet = j.clients.stellar.get("demotest_wallets_wallet")
-        cls.vdc.provision_wallet.merge_into_account(wallet.address)
-        cls.vdc.prepaid_wallet.merge_into_account(wallet.address)
-        super().tearDownClass()
-
     def test_01_deploy_vdc(self):
         """Test case for deploying a VDC.
 
@@ -30,13 +19,17 @@ class VDCChatflows(VDCBase):
 
         - Deploy a VDC.
         - Check that VDC is reachable.
+        - Delete a VDC
         """
 
         self.info("Deploy a VDC")
-        vdc_name = self.random_name()
+        vdc_name = self.random_name().lower()
         password = self.random_string()
         vdc = deployer.deploy_vdc(vdc_name, password, self.flavor.upper())
 
         self.info("Check that VDC is reachable")
-        request = j.tools.http.get(f"http://{vdc.domain}", verify=False, timeout=60)
+        request = j.tools.http.get(f"http://{vdc.vdc.threebot.domain}", verify=False, timeout=60)
         self.assertEqual(request.status_code, 200)
+
+        self.info("Delete a VDC")
+        j.sals.vdc.delete(vdc.vdc.instance_name)
