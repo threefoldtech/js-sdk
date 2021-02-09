206e545beac5c96ac6b2a272ff789ef247b4cc31
Mohammed Essam <mohammedelborolossy@gmail.com>
2021-02-09 16:41:59 +0200

    Development vdc testing dashboard (#2496)
    * update cryptpad chatflow after add a new msg
    
    * Fix teardownClass method

--- solutions_automation/vdc/dashboard/cryptpad.py
+++ solutions_automation/vdc/dashboard/cryptpad.py
@@ -3,4 +3,8 @@ from jumpscale.packages.vdc_dashboard.chats.cryptpad import CryptpadDeploy
 
 
 class CryptpadAutomated(CommonChatBot, CryptpadDeploy):
-    pass
+    STORAGE_SIZE_MESSAGE = "Please select your storage size (in GBs)"
+
+    QS = {
+        STORAGE_SIZE_MESSAGE: "storage_size",
+    }
