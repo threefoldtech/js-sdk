ee4854a85998ece62986c8768b4c0c4dc9cb50cc
Mohammed Essam <mohammedelborolossy@gmail.com>
2021-02-22 10:00:57 +0200

    Update etcd with ingress configurations (#2591)
    * Update etcd to use ingress
    
    * Fix frontend domain button condition
    
    * Enhance get_deployments function using 2 helper functions

--- jumpscale/packages/vdc_dashboard/chats/etcd.py
+++ jumpscale/packages/vdc_dashboard/chats/etcd.py
@@ -1,6 +1,10 @@
+import gevent
 from textwrap import dedent
+from jumpscale.loader import j
 from jumpscale.sals.chatflows.chatflows import chatflow_step
-from jumpscale.packages.vdc_dashboard.sals.solutions_chatflow import SolutionsChatflowDeploy
+from jumpscale.packages.vdc_dashboard.sals.solutions_chatflow import SolutionsChatflowDeploy, POD_INITIALIZING_TIMEOUT
+from jumpscale.packages.vdc_dashboard.sals.vdc_dashboard_sals import get_deployments
+from time import time
 
 CHART_LIMITS = {
     "Silver": {"cpu": "100m", "memory": "128Mi", "no_nodes": "1", "volume_size": "2Gi"},
@@ -20,10 +24,15 @@ class EtcdDeploy(SolutionsChatflowDeploy):
     SOLUTION_TYPE = "etcd"
     title = "ETCD"
     HELM_REPO_NAME = "marketplace"
-    steps = ["get_release_name", "set_config", "install_chart", "success"]
+    steps = ["get_release_name", "create_subdomain", "set_config", "install_chart", "initializing", "success"]
 
     @chatflow_step(title="Configurations")
     def set_config(self):
+        username = self.user_info()["username"]
+        # Check if entrypoint already added before with another deployment
+        if not get_deployments(self.SOLUTION_TYPE, username):
+            self.vdc.get_deployer().kubernetes.add_traefik_entrypoint("etcd", "2379")
+
         self._choose_flavor(CHART_LIMITS, RESOURCE_VALUE_TEMPLATE)
         self.chart_config.update(
             {
@@ -33,29 +42,43 @@ class EtcdDeploy(SolutionsChatflowDeploy):
                 "persistence.size": self.resources_limits["volume_size"],
                 "auth.rbac.enabled": "false",
                 "metrics.enabled": "true",
+                "ingress.host": self.domain,
             }
         )
 
+    @chatflow_step(title="Initializing", disable_previous=True)
+    def initializing(self):
+        error_message_template = f"""\
+                Failed to initialize {self.SOLUTION_TYPE}, please contact support with this information:
+
+                Domain: {self.domain}
+                VDC Name: {self.vdc_name}
+                Farm name: {self.vdc_info["farm_name"]}
+                Reason: {{reason}}
+                """
+        start_time = time()
+        while time() - start_time <= POD_INITIALIZING_TIMEOUT:
+            if self.chart_pods_started():
+                break
+            gevent.sleep(1)
+
+        if not self.chart_pods_started() and self.chart_resource_failure():
+            stop_message = error_message_template.format(
+                reason="Couldn't find resources in the cluster for the solution"
+            )
+            self.k8s_client.delete_deployed_release(self.release_name)
+            self.stop(dedent(stop_message))
+
     @chatflow_step(title="Success", disable_previous=True, final_step=True)
     def success(self):
-
-        replicas_name = [f"<br />-{self.release_name}-{i}" for i in range(int(self.resources_limits["no_nodes"]))]
+        domain_message = f'- You can access it through this url: <a href="https://{self.domain}" target="_blank">https://{self.domain}</a> as follow:<br />\n'
         message = f"""\
         # You deployed a new instance {self.release_name} of {self.SOLUTION_TYPE}
         <br />\n
-        - etcd can be accessed via port 2379 on the following DNS name from within your cluster:
-            {self.release_name}.default.svc.cluster.local
-
-        - Your PODNAME one of the following:
-            {''.join(replicas_name)}
-        - To set a key run the following command:
-            `kubectl exec -it $PODNAME -- etcdctl put message Hello`
-
-        - To get a key run the following command:
-            `kubectl exec -it $PODNAME -- etcdctl get message`
-
-        - To connect to your etcd server from outside the cluster execute the following commands:
-            `kubectl port-forward --namespace default svc/{self.release_name} 2379:2379`
+        {domain_message}
+        `etcdctl --endpoints=https://{self.domain}:2379 put hello Threefold`
+        <br />\n
+        `OK` will appear as confirmation message.
 
         - You can visit <a href="https://etcd.io/docs/v3.4.0/" target="_blank">ETCD Docs</a> for more information
         """
