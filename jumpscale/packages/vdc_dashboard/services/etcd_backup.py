9854850566f630b59bfb77ddf2c5a61177fbb9af
m-motawea <magedmotawea@gmail.com>
2021-02-23 10:45:36 +0200

    read backup config from js config

--- jumpscale/packages/vdc_dashboard/services/etcd_backup.py
+++ jumpscale/packages/vdc_dashboard/services/etcd_backup.py
@@ -1,14 +1,21 @@
 from jumpscale.tools.servicemanager.servicemanager import BackgroundService
 from jumpscale.loader import j
-from jumpscale.packages.backup.actors.threebot_deployer import Backup
+import os
 
 
 class ETCDBackupService(BackgroundService):
-    def __init__(self, interval=60 * 5, *args, **kwargs):
+    def __init__(self, interval=60 * 60 * 24, *args, **kwargs):
         super().__init__(interval, *args, **kwargs)
 
     def job(self):
         # use etcdctl to get a snapshot of etcd and back it up using restic
+        url = os.environ.get("S3_URL")
+        bucket = os.environ.get("S3_BUCKET")
+        ak = os.environ.get("S3_AK")
+        sk = os.environ.get("S3_SK")
+        if not all([url, bucket]):
+            j.logger.warning(f"etcd backup is not configured. url: {url}, bucket: {bucket}")
+            return
         vdc = j.sals.vdc.get(list(j.sals.vdc.list_all())[0])
         vdc.load_info()
         endpoints = ""
@@ -25,8 +32,18 @@ class ETCDBackupService(BackgroundService):
             j.tools.alerthandler.alert_raise("vdc", msg)
             return
         j.logger.info(f"backing up etcd snapshot at location: {snapshot_path} to restic")
-        backup_actor = Backup()
-        backup_actor.backup(paths=[snapshot_path])
+        # backup with restic to s3
+        restic = j.tools.restic.get("vdc")
+        restic.extra_env = {
+            "AWS_ACCESS_KEY_ID": ak,
+            "AWS_SECRET_ACCESS_KEY": sk,
+        }
+        restic.repo = f"s3:https://{url}/{bucket}/{vdc.owner_tname}/{vdc.vdc_name}"
+        restic.password = "mypassword"
+        if not j.core.config.get("RESTIC_INITIALIZED"):
+            restic.init_repo()
+            j.core.config.set("RESTIC_INITIALIZED", True)
+        restic.backup(snapshot_path)
 
 
 service = ETCDBackupService()
