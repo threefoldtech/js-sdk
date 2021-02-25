92408df37a06c0a90021d432b125311e1429f923
dmahmouali <dmahmou213@gmail.com>
2021-02-25 22:40:38 +0200

    Add service name to the logs

--- jumpscale/packages/vdc/services/fund_prices_diff.py
+++ jumpscale/packages/vdc/services/fund_prices_diff.py
@@ -12,10 +12,10 @@ class FundPricesDifference(BackgroundService):
         super().__init__(interval, *args, **kwargs)
 
     def job(self):
-        j.logger.info("Fund prices difference service")
+        j.logger.info(f"FUND PRICES DIFF: Fund prices difference service")
         initialization_wallet_name = j.core.config.get("VDC_INITIALIZATION_WALLET")
         if not initialization_wallet_name:
-            j.logger.critical("No initialization wallet configured on the VDC deployer")
+            j.logger.critical(f"FUND PRICES DIFF: No initialization wallet configured on the VDC deployer")
             return
         initialization_wallet = j.clients.stellar.get(initialization_wallet_name)
         tft = initialization_wallet._get_asset("TFT")
@@ -25,17 +25,19 @@ class FundPricesDifference(BackgroundService):
             vdc_spec_price = vdc_instance.calculate_spec_price()  # user price
             # check if vdc in grace period
             if vdc_instance.is_blocked or vdc_instance.is_empty():
-                j.logger.info(f"VDC {vdc_instance.instance_name} is empty or in grace period")
+                j.logger.info(f"FUND PRICES DIFF: VDC {vdc_instance.instance_name} is empty or in grace period")
                 return
             # check if prepaid have enough money
             prepaid_balance = vdc_instance._get_wallet_balance(vdc_instance.prepaid_wallet)
             if prepaid_balance < vdc_spec_price + TRANSACTION_FEES:
-                j.logger.info(f"VDC {vdc_instance.instance_name} hove not enough money for renew plan")
+                j.logger.info(
+                    f"FUND PRICES DIFF: VDC {vdc_instance.instance_name} hove not enough money for renew plan"
+                )
                 return
             elif prepaid_balance > vdc_spec_price:
                 # TODO: transfer TRANSACTION FEES to the prepaid wallet
                 j.logger.info(
-                    f"VDC {vdc_instance.instance_name} prepaid wallet have the hour cost but don't have the transaction fees {TRANSACTION_FEES} TFT"
+                    f"FUND PRICES DIFF: VDC {vdc_instance.instance_name} prepaid wallet have the hour cost but don't have the transaction fees {TRANSACTION_FEES} TFT"
                 )
                 return
             vdc_real_price = vdc_instance.calculate_active_units_price() * 60 * 60  # explorer price
@@ -46,7 +48,7 @@ class FundPricesDifference(BackgroundService):
                 initialization_wallet.transfer(
                     vdc_instance.provision_wallet.address, diff, asset=f"{tft.code}:{tft.issuer}"
                 )
-                j.logger.info(f"VDC {vdc_instance.instance_name} funded with {diff} TFT")
+                j.logger.info(f"FUND PRICES DIFF: VDC {vdc_instance.instance_name} funded with {diff} TFT")
 
 
 service = FundPricesDifference()
