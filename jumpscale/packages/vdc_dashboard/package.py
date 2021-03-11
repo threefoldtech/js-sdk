15d7a66fbf6a05bff2d905336b8224a9adab661c
ranatrk <rana.t.hassan@gmail.com>
2021-03-11 17:16:30 +0200

    Disable is_package_authorized for controller bottle server of vdc_dashboard

--- jumpscale/packages/vdc_dashboard/package.py
+++ jumpscale/packages/vdc_dashboard/package.py
@@ -7,11 +7,13 @@ class vdc_dashboard:
         location_actors_443 = j.sals.nginx.main.websites.default_443.locations.get(name="vdc_dashboard_controller")
         location_actors_443.is_auth = False
         location_actors_443.is_admin = False
+        location_actors_443.is_package_authorized = False
         location_actors_443.save()
 
         location_actors_80 = j.sals.nginx.main.websites.default_80.locations.get(name="vdc_dashboard_controller")
         location_actors_80.is_auth = False
         location_actors_80.is_admin = False
+        location_actors_80.is_package_authorized = False
         location_actors_80.save()
 
         j.sals.nginx.main.websites.default_443.configure()
