c5c2a1c32d9bcf8562f50f580c363d15e2dca496
samaradel <samar@incubaid.com>
2021-03-15 10:49:47 +0200

    Format timestamp

--- jumpscale/packages/vdc_dashboard/frontend/components/vdcSettings/Backups.vue
+++ jumpscale/packages/vdc_dashboard/frontend/components/vdcSettings/Backups.vue
@@ -166,17 +166,8 @@ module.exports = {
       this.dialogs.actions = true;
     },
     timeDifference(ts) {
-      var a = new Date(ts * 1000);
-      var months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-      var year = a.getFullYear();
-      var month = months[a.getMonth()];
-      var date = a.getDate();
-      var hour = a.getHours();
-      var min = a.getMinutes();
-      var sec = a.getSeconds();
-      var time =
-        date + "/" + month + "/" + year + " " + hour + ":" + min + ":" + sec;
-      return time;
+      var timestamp = new Date(ts * 1000).toLocaleString("en-GB");
+      return timestamp
     },
     getStatus(status) {
       if (status == "Error") return "red";
