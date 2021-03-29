ae21e867f87f1df678721978eebbce0d5947b808
abdulgig <a.rehmans@gmail.com>
2021-03-29 13:28:19 +0000

    Add matic chatflow for validator

--- jumpscale/packages/vdc_dashboard/chats/matic.py
+++ jumpscale/packages/vdc_dashboard/chats/matic.py
@@ -5,7 +5,7 @@ from jumpscale.packages.vdc_dashboard.sals.vdc_dashboard_sals import get_deploym
 
 class MaticDeploy(SolutionsChatflowDeploy):
     SOLUTION_TYPE = "matic"
-    title = "Matic"
+    title = "Polygon"
     HELM_REPO_NAME = "marketplace"
     steps = ["get_release_name", "create_subdomain", "set_config", "install_chart", "initializing", "success"]
 
@@ -14,6 +14,22 @@ class MaticDeploy(SolutionsChatflowDeploy):
         "Gold": {"cpu": "4000m", "memory": "4096Mi"},
         "Platinum": {"cpu": "4000m", "memory": "8192Mi"},
     }
+
+    def _enter_nodetype(self):
+        choices = [
+            "Sentry Node",
+            "Full Node",
+            "Validator",
+        ]
+        self.node_type = self.single_choice("Select the Node Type", choices, default="Sentry Node")
+        if self.node_type == "Full Node":
+            self._enter_rpc_url()
+        elif self.node_type == "Validator":
+            self._enter_rpc_url()
+            self._enter_validator_info()
+            self._enter_sentry_info()
+        else:
+            pass
 
     def _check_uniqueness(self):
         username = self.user_info()["username"]
@@ -27,27 +43,39 @@ class MaticDeploy(SolutionsChatflowDeploy):
 
     def _enter_access_code(self):
         self.access_code = self.secret_ask(
-            "Enter Access Code (This would be used to access your node's web page)", required=True
+            "Enter Access Code (This would be used to access your node's web page)", min_length=8, required=True
         )
 
     def _enter_rpc_url(self):
-        self.eth_rpc_url = self.string_ask(
-            "Insert Infura or any full node RPC URL to Ethereum. You can skip this option if you are deploying a sentry node"
-        )
+        self.eth_rpc_url = self.string_ask("Insert Infura or any full node RPC URL to Ethereum.", required=True,)
+        self.chart_config.update({"env.eth_rpc_url": self.eth_rpc_url})
 
-    def _enter_eth_keyinfo(self):
-        self.eth_key = self.string_ask(
-            "For Validator setup, please enter your ethereum wallet's *private* key. You can skip this step if you are setting up a full node or a sentry node."
-        self.eth_key_passphrase = self.string_ask(
-            "Please enter the passphrase for your ethereum wallet's *private* key. It should be minimum 8 characters."
-        self.eth_walletaddr = self.string_ask(
-            "Please enter your Ethereum wallet address"
+    def _enter_validator_info(self):
+        form = self.new_form()
+        self.eth_privkey = form.string_ask("Please enter your ethereum wallet's *private* key.", required=True)
+        self.eth_key_passphrase = form.secret_ask(
+            "Please enter the passphrase for your ethereum wallet's *private* key. It should be minimum 8 characters.",
+            min_length=8,
+            required=True,
         )
+        self.eth_walletaddr = form.string_ask("Please enter your ethereum wallet address.", required=True)
+        form.ask()
+        self.eth_privkey = self.eth_privkey.value
+        self.eth_key_passphrase = self.eth_key_passphrase.value
+        self.eth_walletaddr = self.eth_walletaddr.value
+
+    def _enter_sentry_info(self):
+        form = self.new_form()
+        self.sentry_nodeid = form.string_ask("Please enter your sentry node's NodeID for heimdall", required=True)
+        self.sentry_enodeid = form.string_ask("Please enter your sentry node's EnodeID for Bor", required=True)
+        form.ask()
+        self.sentry_nodeid = self.sentry_nodeid.value
+        self.sentry_enodeid = self.sentry_enodeid.value
 
     @chatflow_step(title="Node Configuration")
     def set_config(self):
+        self._enter_nodetype()
         self._enter_access_code()
-        self._enter_rpc_url()
         self._choose_flavor(self.CHART_LIMITS)
         self.vdc.get_deployer().kubernetes.add_traefik_entrypoints(
             {"matic-bor": {"port": "30303"}, "matic-heimdall": {"port": "26656"}}
@@ -56,7 +84,6 @@ class MaticDeploy(SolutionsChatflowDeploy):
         self.chart_config.update(
             {
                 "access_code": self.access_code,
-                "env.eth_rpc_url": self.eth_rpc_url,
                 "global.ingress.host": self.domain,
                 "resources.limits.cpu": self.resources_limits["cpu"],
                 "resources.limits.memory": self.resources_limits["memory"],
