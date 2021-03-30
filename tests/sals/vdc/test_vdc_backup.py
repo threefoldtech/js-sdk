4fb01ed62af635cddeedfa5caaa5ab0ab9f6da3f
Mostafa Hassan <mhost39@gmail.com>
2021-03-30 14:00:07 +0200

    update packup vdc

--- tests/sals/vdc/test_vdc_backup.py
+++ tests/sals/vdc/test_vdc_backup.py
@@ -1,4 +1,6 @@
-import gevent
+import os
+
+from gevent import sleep
 import pytest
 from jumpscale.clients.explorer.models import K8s
 from jumpscale.loader import j
@@ -8,75 +10,48 @@ from tests.sals.vdc.vdc_base import VDCBase
 from tests.sals.automated_chatflows.chatflows_base import ChatflowsBase
 from parameterized import parameterized_class
 from jumpscale.clients.stellar import TRANSACTION_FEES
+from solutions_automation.vdc import deployer
 
 
-@parameterized_class(("no_deployment"), [("single",)])
 @pytest.mark.integration
 class VDCDashboard(VDCBase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls._import_wallet("demos_wallet")
-        cls.flavor = "silver"
+        cls._import_wallet(wallet_name="vdc_init")
+        cls._import_wallet(wallet_name="grace_period")
+        cls.config_vdc = j.core.config.get("VDC_INITIALIZATION_WALLET")
+        j.core.config.set("VDC_INITIALIZATION_WALLET", "vdc_init")
 
-        cls.vdc_name = cls.random_name().lower()
-        cls.password = cls.random_string()
-        cls.kube_config = cls.deploy_vdc(cls.vdc_name, cls.password)
-
-        if not cls.kube_config:
-            raise RuntimeError("VDC is not deployed")
-
-        j.sals.fs.copy_file(
-            f"{j.sals.fs.home()}/sandbox/cfg/vdc/kube/{cls.vdc.owner_tname}/{cls.vdc.vdc_name}.yaml",
-            j.sals.fs.expanduser("~/.kube/config"),
-        )
-        cls.kube_manager = j.sals.kubernetes.Manager()
-
-        cls.info("Check that resources are available and ready in 5 min maximum")
-        cls.kube_monitor = cls.vdc.get_kubernetes_monitor()
-        has_resource = False
-        resources_expiry = j.data.time.now().timestamp + 300
-        while j.data.time.now().timestamp < resources_expiry and not has_resource:
-            res = cls.kube_monitor.fetch_resource_reservations()
-            for node in res:
-                if node.has_enough_resources(1000, 1024):
-                    cls.info("Cluster resources are ready")
-                    has_resource = True
-                    break
-
-        cls.timeout = 60
-        # Accept Marketplace T&C for testing identity.
-        ChatflowsBase.tname = cls.tname
-        ChatflowsBase.accept_terms_conditions(type_="marketplace")
+        backup_vars = ["S3_URL", "S3_BUCKET", "S3_AK", "S3_SK"]
+        for var in backup_vars:
+            value = os.environ.get(var)
+            if not value:
+                raise ValueError(f"Please add {var} as environment variables")
+            setattr(cls, var, value)
+        s3_local_config = {"S3_URL": cls.S3_URL, "S3_BUCKET": cls.S3_BUCKET, "S3_AK": cls.S3_AK, "S3_SK": cls.S3_SK}
+        j.config.set("VDC_S3_CONFIG", s3_local_config)
 
     @classmethod
     def tearDownClass(cls):
-        wallet = j.clients.stellar.get("demos_wallet")
-        cls.vdc.provision_wallet.merge_into_account(wallet.address)
-        cls.vdc.prepaid_wallet.merge_into_account(wallet.address)
-        j.sals.vdc.delete(cls.vdc.instance_name)
+        if cls.config_vdc:
+            j.core.config.set("VDC_INITIALIZATION_WALLET", cls.config_vdc)
+        j.config.set("VDC_S3_CONFIG", {})
         super().tearDownClass()
 
-    def _get_and_wait_ssl(self, domain, expire_timeout=180):
-        expiry = j.data.time.now().timestamp + expire_timeout
-        is_certified = False
-        request = None
-        while j.data.time.now().timestamp < expiry:
-            try:
-                request = j.tools.http.get(domain, timeout=self.timeout)
-                is_certified = True
-                break
-            except j.tools.http.exceptions.SSLError:
-                self.info("Waiting to check ssl certificate....")
-                gevent.sleep(10)
+    def tearDown(self):
+        self.info("Delete a VDC")
+        if self.vdc:
+            vdc = self.vdc.vdc
+        else:
+            vdc = j.sals.vdc.get(f"vdc_{self.vdc_name}_{self.tname}")
 
-        if not is_certified:
-            j.logger.warning(
-                f"{domain} can't gain a ssl certificate, it maybe take longer than {expire_timeout/60} min to resolve it, check again later!"
-            )
-            request = j.tools.http.get(domain, timeout=self.timeout, verify=False)
+        j.sals.vdc.delete(vdc.instance_name)
+        wallet = j.clients.stellar.get("demos_wallet")
+        vdc.provision_wallet.merge_into_account(wallet.address)
+        vdc.prepaid_wallet.merge_into_account(wallet.address)
 
-        return request
+        super().tearDown()
 
     def create_backup(self, backup_name):
         config_path = j.sals.fs.expanduser("~/.kube/config")
@@ -106,14 +81,32 @@ class VDCDashboard(VDCBase):
 
         **Test Scenario**
 
+        - Prepare ssh key
         - Deploy VDC.
         - Deploy a cryptpad.
         - Check that cryptpad has been reachable.
+        - Up wireguard
         - Backup a VDC.
         - Delete this cryptpad.
         - Restore backup.
         - Check that cryptpad reachable.
         """
+
+        self.info("Prepare ssh key")
+        self.ssh_client_name = self.random_name()
+        if not j.sals.fs.exists("/tmp/.ssh"):
+            j.core.executors.run_local('mkdir /tmp/.ssh && ssh-keygen -t rsa -f /tmp/.ssh/id_rsa -q -N "" ')
+        self.ssh_cl = j.clients.sshkey.get(self.ssh_client_name)
+        self.ssh_cl.private_key_path = "/tmp/.ssh/id_rsa"
+        self.ssh_cl.load_from_file_system()
+        self.ssh_cl.save()
+
+        self.info("Deploy a VDC")
+        self.vdc_name = self.random_name().lower()
+        password = self.random_string()
+        flavor = "silver"
+        self.vdc = deployer.deploy_vdc(self.vdc_name, password, flavor.upper())
+
         self.info("Deploy a Cryptpad.")
         name = self.random_name().lower()
         cryptpad = deployer.deploy_cryptpad(release_name=name)
@@ -123,16 +116,32 @@ class VDCDashboard(VDCBase):
         request = j.tools.http.get(url=f"https://{cryptpad.domain}", timeout=180, verify=False)
         self.assertEqual(request.status_code, 200)
 
+        self.info("Up wireguard")
+        threebot = self.vdc.vdc.threebot
+        self.wg_conf_paths.append(f"/tmp/{self.random_name()}.conf")
+        j.sals.fs.write_file(self.wg_conf_paths[-1], threebot.wgcfg)
+        rc, out, err = j.sals.process.execute(f"sudo wg-quick up {self.wg_conf_paths[-1]}")
+        sleep(5)
+
         self.info("Backup a VDC")
+        localclient = j.clients.sshclient.get(self.ssh_client_name)
+        localclient.sshkey = self.ssh_client_name
+        localclient.host = threebot.ip_address
+        localclient.save()
+
         backup_name = self.random_name().lower()
-        self.create_backup(backup_name)
+        _, res, _ = localclient.sshclient.run(
+            f"velero create backup config-{backup_name} --include-resources secrets,configmaps"
+        )
+        _, res, _ = localclient.sshclient.run(f'velero create backup vdc-{backup_name} -l "backupType=vdc"')
 
         self.info("Delete this cryptpad")
-        kube_manager = j.sals.kubernetes.Manager()
-        kube_manager.execute_native_cmd(f"kubectl delete ns {cryptpad.chart_name}-{cryptpad.release_name}")
+        # how delete cryptpad
 
         self.info("Restore backup")
-        self.restore_backup(backup_name)
+        _, res, _ = localclient.sshclient.run(
+            f"velero create restore restore-vdc-{backup_name} --from-backup vdc-{backup_name}"
+        )
 
         self.info("Check that cryptpad has been reachable")
         request = j.tools.http.get(url=f"https://{cryptpad_domain}", timeout=600, verify=False)
