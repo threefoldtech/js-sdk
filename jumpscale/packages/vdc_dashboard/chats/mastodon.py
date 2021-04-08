1482505140ff03935d68b0d3331a48debc111db5
sameh Abouel-saad <samehabouelsaad@gmail.com>
2021-04-08 15:35:22 +0000

    fix smtp settings

--- jumpscale/packages/vdc_dashboard/chats/mastodon.py
+++ jumpscale/packages/vdc_dashboard/chats/mastodon.py
@@ -34,9 +34,10 @@ class MastodonDeploy(SolutionsChatflowDeploy):
                 "resources.limits.memory": self.resources_limits["memory"][:-2],
                 "env.smtp.server": self.smtp_host,
                 "env.smtp.port": self.smtp_port.strip('"'),
-                "env.smtp.address": self.smtp_host,
+                "env.smtp.address": f"support@{self.domain}",
                 "env.smtp.login": self.smtp_username,
                 "env.smtp.domain": self.domain,
+		"smtpPassword": self.smtp_password,
                 "env.extraEnvVars[0].name": "THREEBOT_KEY",
                 "env.extraEnvVars[0].value": self.generate_signing_key(),
             }
