7acafe6e3bceece051780ef59306273ddb825b45
Waleed <waleed.hammam@gmail.com>
2021-04-16 21:47:09 +0200

    extend the usage of reporting tool to take all available farms or selection (#2955)

--- jumpscale/tools/zos/reporting/__init__.py
+++ jumpscale/tools/zos/reporting/__init__.py
@@ -70,7 +70,6 @@ from jumpscale.loader import j
 from jumpscale.sals.zos.node_finder import NodeFinder
 
 
-PUBLIC_IP_FARMS = {"devnet": "lochristi_dev_lab", "testnet": "freefarm", "mainnet": "freefarm"}
 DEFAULT_EXPLORER_URLS = {
     "mainnet": "https://explorer.grid.tf/api/v1",
     "testnet": "https://explorer.testnet.grid.tf/api/v1",
@@ -78,38 +77,55 @@ DEFAULT_EXPLORER_URLS = {
 }
 
 
-def get_public_ip_usage(explorer_name: str = "devnet"):
+def get_public_ip_usage(explorer_name: str = "devnet", farms_names=None):
     """Provide a list of the used/free public ips
 
     Args:
         explorer (str, optional): explorer name should be in ["devnet", "testnet", "mainnet"]. Defaults to "devnet".
-
+        farms_name (list, optional): farm name / list of farms names to search in, None will get all values
     Returns:
         str: nice view for the used ips or json
     """
     print("\nGetting public ip usage ...")
     explorer = j.clients.explorer.get_by_url(DEFAULT_EXPLORER_URLS[explorer_name])
-    c = 0
-    ip_usage = [["Username", "Owner tid", "WID"]]
+
     if not explorer_name in ["devnet", "testnet", "mainnet"]:
         raise j.exceptions.Value(f"Explorers: devnet, testnet, mainnet are only supported, you passed {explorer_name}")
-    farm = explorer.farms.get(farm_name=PUBLIC_IP_FARMS[explorer_name])
-    for ip in farm.ipaddresses:
-        if not ip.reservation_id:
-            continue
-        c += 1
-        workload = explorer.workloads.get(ip.reservation_id)
-        owner_tid = workload.info.customer_tid
-        user = explorer.users.get(owner_tid)
-        ip_usage.append([user.name, owner_tid, workload.id])
-    ip_usage.append(["---------------------------------------------------------------", "--------", "--------"])
-    ip_usage.append([f"Busy: {c}", f"Free: {len(farm.ipaddresses) - c}", f"Total: {len(farm.ipaddresses)}"])
-    j.data.terminaltable.print_table(
-        j.tools.console.printcolors(
-            f"\nReserved public IPs on Farm: {{GREEN}}{farm.name}{{RESET}} in Explorer: {{GREEN}}{explorer_name}{{RESET}}\n"
-        ),
-        ip_usage,
-    )
+
+    farms_with_public_ip = []
+    if farms_names:
+
+        if not isinstance(farms_names, list):
+            farms_names = [farms_names]
+
+        for farm_name in farms_names:
+            try:
+                farm = explorer.farms.get(farm_name=farm_name)
+                farms_with_public_ip.append(farm)
+            except Exception as e:
+                j.logger.error(f"No farm with the name {farm_name} exists in explorer {explorer_name} due to {str(e)}")
+
+    farms = farms_with_public_ip or [farm for farm in explorer.farms.list() if farm.ipaddresses]
+
+    for farm in farms:
+        c = 0
+        ip_usage = [["Username", "Owner tid", "WID"]]
+        for ip in farm.ipaddresses:
+            if not ip.reservation_id:
+                continue
+            c += 1
+            workload = explorer.workloads.get(ip.reservation_id)
+            owner_tid = workload.info.customer_tid
+            user = explorer.users.get(owner_tid)
+            ip_usage.append([user.name, owner_tid, workload.id])
+        ip_usage.append(["---------------------------------------------------------------", "--------", "--------"])
+        ip_usage.append([f"Busy: {c}", f"Free: {len(farm.ipaddresses) - c}", f"Total: {len(farm.ipaddresses)}"])
+        j.data.terminaltable.print_table(
+            j.tools.console.printcolors(
+                f"\nReserved public IPs on Farm: {{GREEN}}{farm.name}{{RESET}} in Explorer: {{GREEN}}{explorer_name}{{RESET}}\n"
+            ),
+            ip_usage,
+        )
 
 
 def get_available_farm_capacity(explorer_name: str = "devnet", farm_name: str = "lochristi_dev_lab"):
