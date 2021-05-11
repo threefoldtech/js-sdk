a08753bb88ba78c222162aae0c9eae1d8b970c72
Sameh Abouelsaad <samehabouelsaad@gmail.com>
2021-05-11 13:10:19 +0200

    refactor: implementing @xmonader suggestion

--- jumpscale/packages/vdc_dashboard/scripts/update.sh
+++ jumpscale/packages/vdc_dashboard/scripts/update.sh
@@ -1,11 +1,16 @@
 #################
-# Exit Code Number    Meaning
+# Exit Code Number    Meaning                                                   
 # 0                   Okay
-# 64                  Error: poetry install failed
-# 65                  Error: hard reset files to HEAD failed
-# 66                  Error: switch to the upstream branch failed
-# 67                  Error: can't find the branch name in the repository
+# 64                  Error: poetry install failed                              POETRY_INSTALL_ERROR
+# 65                  Error: hard reset files to HEAD failed                    GIT_RESET_ERROR
+# 66                  Error: switch to the upstream branch failed               GIT_CHECKOUT_ERROR
+# 67                  Error: can't find the branch name in the repository       GIT_BRANCH_NOT_EXIST
 #################
+POETRY_INSTALL_ERROR=64
+GIT_RESET_ERROR=65
+GIT_CHECKOUT_ERROR=66
+GIT_BRANCH_NOT_EXIST=67
+
 # fetching all remote branches"
 git fetch --all
 
@@ -16,7 +21,7 @@ echo "we will use origin/${SDK_BRANCH} branch to update the code"
 # find out if a remote git branch with $SDK_BRANCH name exists in the repository
 if ! git show-ref --quiet --verify "refs/remotes/origin/$SDK_BRANCH"; then
   >&2 echo "error: can't find the branch <$SDK_BRANCH> in the repository"
-  exit 64
+  exit $GIT_BRANCH_NOT_EXIST
 fi
 
 # save the current information to use in case of update failure
@@ -34,24 +39,24 @@ fi
 
 git stash push jumpscale/packages/vdc_dashboard/package.toml
 
-# git ride of the uncommited local changes and switch the branch
+# git rid of the uncommited local changes and switch the branch
 if ! git checkout -f $SDK_BRANCH; then
   >&2 echo "error: switch to the upstream branch <$SDK_BRANCH> failed"
   git stash pop
-  exit 65
+  exit $GIT_CHECKOUT_ERROR
 fi
 
 RESET_SUCCEEDED=false
 if git reset --hard origin/$SDK_BRANCH; then
   RESET_SUCCEEDED=true
 else
-  ERR=66
+  ERR=$GIT_RESET_ERROR
   >&2 echo "Error: failed to hard reset files to HEAD"
 fi
 
 if $RESET_SUCCEEDED; then
   if $POETRY_INSTALL; then
-    ERR=67
+    ERR=$POETRY_INSTALL_ERROR
     MAX_TRIES=4
     COUNT=0
     while [  $COUNT -lt $MAX_TRIES ]; do
@@ -71,7 +76,7 @@ if $RESET_SUCCEEDED; then
   fi
 fi
 
-# revert back to saved info if hard reset files to HEAD or installing the defined dependencies was failed
+# reverting to saved info if hard reset files to HEAD or installing the defined dependencies was failed
 git checkout -f $SAVED_BRANCH_NAME && git reset --hard $SAVED_COMMIT_HASH
 git stash pop
 exit $ERR
