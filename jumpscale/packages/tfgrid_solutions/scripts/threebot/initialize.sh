27b503d52f3f766759cafff0ea6b9dbaabd87120
Omar Elawady <omarelawady1998@gmail.com>
2021-06-01 18:24:30 +0200

    remove pre-downloaded packages

--- jumpscale/packages/tfgrid_solutions/scripts/threebot/initialize.sh
+++ jumpscale/packages/tfgrid_solutions/scripts/threebot/initialize.sh
@@ -1,16 +1,7 @@
 echo "[*] Starting threebot in background ..."
 
 if [ $MINIMAL == "true" ]; then
-  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-  chmod +x kubectl
-  mv kubectl /sbin/
-  curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-  wget https://github.com/vmware-tanzu/velero/releases/download/v1.5.3/velero-v1.5.3-linux-amd64.tar.gz
-  tar -xvf velero-v1.5.3-linux-amd64.tar.gz
-  chmod +x velero-v1.5.3-linux-amd64/velero
-  mv velero-v1.5.3-linux-amd64/velero /sbin/
-  rm -rf velero-v1.5.3-linux-amd64
-  apt install etcd-client -y
+
   python3 jumpscale/packages/tfgrid_solutions/scripts/threebot/minimal_entrypoint.py
 else
   echo "INSTANCE_NAME=${INSTANCE_NAME}" >> ~/.bashrc
