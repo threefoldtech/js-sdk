1d6699634fd3e645493f5abdbacac339eae5d527
MElborolossy <mohammedelborolossy@gmail.com>
2021-06-24 18:37:11 +0200

    Add logs

--- jumpscale/install/certbot/certbot_cronjob.py
+++ jumpscale/install/certbot/certbot_cronjob.py
@@ -32,30 +32,41 @@ def check_managed_certificate(certbot):
         j.logger.error(f"Check certificate failed {out}\n{err}")
         return False
     elif out.count("No certificates found") > 0:
-        j.logger.error(f"No certificate managed by certbot for {certbot.domain}")
+        j.logger.info(f"No certificate managed by certbot for {certbot.domain}")
         return False
 
+    j.logger.info(f"Certificate managed by certbot for {certbot.domain}")
     return True
 
 
+j.logger.info("Start Certbot Cronjob")
 threebot_server = j.servers.threebot.get("default")
+renew_command = []
 for p in threebot_server.packages.list_all():
     package = threebot_server.packages.get(p)
     current_nginx_cert = package.nginx_config.nginx.cert  # To revert it again
     package.nginx_config.nginx.cert = False  # To disable generate a certificate
     package.nginx_config.apply()
-    renew_command = []
+    j.logger.debug(f"Check Package:{p}")
     for w in package.nginx_config.nginx.websites.list_all():
         website = package.nginx_config.nginx.websites.get(w)
-        certbot = website.certbot
         if website.domain:
+            certbot = website.certbot
             if check_managed_certificate(certbot):
                 # Certificate managed by certbot, Execute renew after check all certificates
-                if not renew_cmd:
+                if not renew_command:  # We need to run it one time to get renew command
                     renew_command = renew_cmd(certbot)
                 continue
             else:
                 # Certifcate not managed by certbot, Run certbot to get a new one
+                j.logger.info("New certificate will created to be managed by certbot")
                 website.obtain_and_install_certifcate()
 
-    rc, out, err = j.sals.process.exceute(renew_command)
+j.logger.info("Excute certbot renew to renew all the managed certificates")
+j.logger.info(f"{' '.join(renew_command)}")
+rc, out, err = j.sals.process.execute(renew_command)
+
+if rc > 0:
+    j.logger.error(f"Check certificate failed {out}\n{err}")
+else:
+    j.logger.info(f"Certificates Renewed\n{out}")
