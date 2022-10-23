452a84bc0561ff2c027ea249df0558f2f79e01cd
Abdelrahman Ghanem <abom.jdev@gmail.com>
2022-10-23 15:15:33 +0200

    delete zos/vdc reporting tools, explorer client and marketplace package

--- jumpscale/clients/explorer/base.py
+++
@@ -1,8 +0,0 @@
-class BaseResource:
-    def __init__(self, client):
-        self._client = client
-        self._session = client._session
-
-    @property
-    def _url(self):
-        return self._client.url + f"/{self._resource}"
--- jumpscale/packages/marketplace/bottle/models.py
+++
@@ -1,8 +0,0 @@
-from jumpscale.core.base import Base, fields
-from jumpscale.loader import j
-
-
-class UserEntry(Base):
-    explorer_url = fields.String()
-    tname = fields.String()
-    has_agreed = fields.Boolean(default=False)
